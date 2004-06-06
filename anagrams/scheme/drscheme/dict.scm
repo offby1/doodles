2ae7a59330af0d7d5a4e9e6f4484cd40e471f9b1
Eric Hanchrow <eric.hanchrow@gmail.com>
2004-06-06 20:04:14 +0000

    * don't bother caching the dictionary, since it doesn't seem to save any time * whop current-output-port so it sends its characters to our window

--- anagrams/scheme/drscheme/dict.scm
+++ anagrams/scheme/drscheme/dict.scm
@@ -1,16 +1,14 @@
 (module dict
-    mzscheme
+  mzscheme
   (require "bag.scm"
            (prefix list- (lib "list.ss"))
            (lib "pretty.ss")
            (lib "process.ss")
            (prefix srfi-1- (lib "1.ss" "srfi"))
            (prefix srfi-13- (lib "13.ss" "srfi")))
-  (provide dict-for-each init test-init)
-
+  (provide dict-for-each init)
+  
   (define *big-ol-hash-table* #f)
-
-  (define *alist-file-name* "big-dict-alist.scm")
 
   (define (wordlist->hash)
     (with-input-from-file
@@ -21,111 +19,70 @@
           (let loop ((word  (read-line)))
             (if (eof-object? word)
                 (printf (format "done: ~a~%" dict))
-              (begin
-                (if (word-acceptable? word)
-                    (adjoin-word dict (srfi-13-string-downcase word)))
-                (loop (read-line )))
-              ))
+                (begin
+                  (if (word-acceptable? word)
+                      (adjoin-word dict (srfi-13-string-downcase word)))
+                  (loop (read-line )))
+                ))
           dict)
         )))
   
   (define *alist* #f)
-
+  
   (define (dict-for-each proc)
     "An iterator over dictionary elements.  Applies PROC
 successively on all dictionary items.  The arguments to PROC are
 \"(key value)\" where key and value are successive pairs from the
 dictionary."
-    (if #f
-        (hash-table-for-each proc *big-ol-hash-table*)
-      (for-each (lambda (p)
-                  (proc (car p)
-                        (cdr p))) *alist*)))
-
+    (for-each (lambda (p)
+                (proc (car p)
+                      (cdr p))) *alist*))
+  
   (define (adjoin-word dict word)
     (let* ((this-bag (bag word))
            (probe (hash-table-get dict this-bag (lambda () #f))))
       (cond
-       ((not probe)
-        (hash-table-put! dict this-bag (list word)))
-       ((not (member word probe))
-        (hash-table-put! dict this-bag (cons word probe)))
-       )))
-
+        ((not probe)
+         (hash-table-put! dict this-bag (list word)))
+        ((not (member word probe))
+         (hash-table-put! dict this-bag (cons word probe)))
+        )))
+  
   (define word-acceptable?
     (let ((has-vowel-regexp (regexp "[aeiouAEIOU]"))
           (has-non-ASCII-regexp (regexp "[^a-zA-Z]"   )))
       (lambda (word)
         (let ((l (string-length word)))
           (and (not (zero? l))
-
+               
                ;; it's gotta have a vowel.
                (regexp-match has-vowel-regexp word)
-
+               
                ;; it's gotta be all ASCII, all the time.
                (not (regexp-match has-non-ASCII-regexp word))
-             
+               
                ;; it's gotta be two letters long, unless it's `i' or `a'.
                (or (string=? "i" word)
                    (string=? "a" word)
                    (< 1 l)))))))
   
-
+  
   (define (bag-acceptable? this bag-to-meet)
     (and (or (bags=? bag-to-meet this)
              (subtract-bags bag-to-meet this))
          this))
-
+  
   (define (init bag-to-meet)
-    (set! *big-ol-hash-table* (make-hash-table 'equal))
-
-    (if (not (file-exists? *alist-file-name*))
-        (begin
-          (printf (format "Caching dictionary ... "))
-          ;; don't use `call-with-input-file',since pretty-print doesn't
-          ;; take a port argument in guile 1.6.4
-          (call-with-output-file *alist-file-name*
-            (lambda (op)
-              ;; use `pretty-print' rather than `write' so that it's not all
-              ;; on on big line.
-              (pretty-print (hash-table-map (wordlist->hash) 
-                                            (lambda (key value)
-                                              (cons key value)))
-                            op)))
-          
-          (printf (format "done~%"))))
-
-    (printf (format "Pruning dictionary ... ")) (flush-output)
-    (for-each
-     (lambda (pair)
-       (let ((bag (car pair))
-             (words (cdr pair)))
-         (let ((this-bag (bag-acceptable? bag bag-to-meet)))
-           (if this-bag
-               (hash-table-put! *big-ol-hash-table* bag words)))))
-     (with-input-from-file *alist-file-name* read))
-    ;; now clobber the alist.
-    (set! *alist* (hash-table-map *big-ol-hash-table* 
-                             
-                                  (lambda (key value )
-                                    (cons key value))))
-    (printf (format "done~%"))
-
+    (if (not *big-ol-hash-table*)
+        (set! *big-ol-hash-table* (wordlist->hash)))
     
-    ;; TODO -- consider sorting *alist*.  There are two ways you might
-    ;; want to sort it:
+    (printf (format "Pruning dictionary ... ")) (flush-output)
 
-    ;; 1) put the  big words first, so that  the most interesting anagrams
-    ;; appear first.
-
-    ;; 2) shuffle randomly, so that when we run the program many times
-    ;; with the same input, the anagrams appear in different orders.  This
-    ;; is useful for when the input is large, and the complete list of
-    ;; anagrams would take forever to generate; in that case, we simply
-    ;; let it run for a little while and collect the first few it
-    ;; generates.  If it generates different ones each run, we'll not get
-    ;; bored.
-
+    (set! *alist* (srfi-1-filter (lambda (entry)
+                                   (bag-acceptable? (car entry) bag-to-meet))
+                          (hash-table-map *big-ol-hash-table* cons)))
+    (printf (format "done~%"))
+    
     (let ()
       (define (biggest-first e1 e2) 
         (let* ((s1 (cadr e1))
@@ -142,26 +99,18 @@ dictionary."
                                         seq)
                                    ))
           (set! with-random-numbers (list-quicksort with-random-numbers (lambda (a b)
-                                                                     (< (car a)
-                                                                        (car b)))))
+                                                                          (< (car a)
+                                                                             (car b)))))
           (map cdr with-random-numbers)))
-
+      
       (printf (format "Sorting or shuffling the dictionary (~a elements), for the hell of it ... "
-              (length *alist*)))
+                      (length *alist*)))
       (set! *alist* 
             (if #t
                 (list-quicksort *alist* biggest-first)
-              (shuffled *alist*))
+                (shuffled *alist*))
             )
       (printf (format "done.~%")))
     )
-
-  (define (test-init)
-    (set! *big-ol-hash-table* (make-hash-table 'equal))
-    (for-each (lambda (word)
-                (hash-table-put!
-                 *big-ol-hash-table*
-                 (bag word)
-                 (list word)))
-              (map (lambda (i)
-                     (string (integer->char (+ i (char->integer #\a))))) (srfi-1-iota 26)))))
+  
+)
--- anagrams/scheme/drscheme/graphical.scm
+++ anagrams/scheme/drscheme/graphical.scm
@@ -1,55 +1,77 @@
+;;; Further ideas
+
+;;; * pause, cancel, continue buttons.
+;;; * keep track of the number of anagrams generated so far, and display it somewhere.
+;;; * likewise for the elapsed time.
+;;; * more feedback when reading the dictionary.
+;;; * bind current-output-port to something that puts the characters somewhere in the window.1
+
 (module graphical
-  mzscheme
+    mzscheme
   (require (lib "mred.ss" "mred"))
   (require (lib "class.ss"))
   (require "anagrams.scm")
-  (provide main)
-  (define (string-append-with-spaces words)
-    (let loop ((result "")
-               (words words))
-      (cond
-       ((null? words)
-        result)
-       (#t
-        (loop (string-append
-               result
-               (if (zero? (string-length result))
-                   ""
-                 " ")
-               (car words))
-              (cdr words))))))
-  (define (main)
-    (define f (instantiate frame% ("Anagrams Redux")))
-    
-    (define input
-      (instantiate text-field% ()
-        (parent f)
-        (label "Enter something to anagram:")
-        (callback 
-         (lambda (object event)
-           (when (eq? 'text-field-enter (send event get-event-type ))
-             (send object enable #f)
-             (let* ((input-string  (send input get-value))
-                    (output (instantiate text-field% ()
-                              (parent f)
-                              (label (format "anagrams of ~s" input-string))
-                              (style '(multiple ))
-                              (enabled #f) ; #f is overkill.  I merely
-                                           ; want to prevent the user
-                                           ; from changing the
-                                           ; contents, but this also
-                                           ; prevents him from
-                                           ; selecting, scrolling,
-                                           ; etc.
-                              (callback (lambda args #f))))
-                    )
-               (let ((editor (send output get-editor)))
-                 (all-anagrams-mit-callback 
-                  input-string 
-                  (lambda (an)
-                    (send editor insert (format "~a~%" (string-append-with-spaces an)))
-                    (yield)))))
-             (send object enable #t))))))
-    (send f show #t))
-  (main))
 
+  (define f (instantiate frame% ("Anagrams Redux")))
+  (define input
+    (instantiate 
+     text-field% ()
+     (parent f)
+     (label "Enter something to anagram:")
+     (callback 
+      (lambda (object event)
+        (when (eq? 'text-field-enter (send event get-event-type ))
+          (send object enable #f)
+          (let* ((input-string  (send input get-value))
+                 (output (instantiate text-field% ()
+                                      (parent f)
+                                      (label (format "anagrams of ~s" input-string))
+                                      (style '(multiple ))
+                                      (enabled #t) ; #f is overkill.  I merely
+                                        ; want to prevent the user
+                                        ; from changing the
+                                        ; contents, but this also
+                                        ; prevents him from
+                                        ; selecting, scrolling,
+                                        ; etc.
+                                      (callback (lambda args #f))))
+                     
+                 (editor (send output get-editor)))
+                
+            
+            (parameterize
+             ((current-output-port
+               (make-custom-output-port
+                #f
+                (lambda (s start end buffer-ok?)
+                  (send output enable #f)
+                  ;; todo -- move point to end before inserting
+                  (send editor insert (substring s start end))
+                  (send output enable #t)
+                  (yield)
+                  (- end start))
+                void
+                void)))
+             
+             (all-anagrams-mit-callback 
+              input-string 
+              (lambda (an)
+
+                (define (string-append-with-spaces words)
+                  (let loop ((result "")
+                             (words words))
+                    (cond
+                     ((null? words)
+                      result)
+                     (#t
+                      (loop (string-append
+                             result
+                             (if (zero? (string-length result))
+                                 ""
+                               " ")
+                             (car words))
+                            (cdr words))))))
+               
+                (printf (format "~a~%" (string-append-with-spaces an)))))))
+          (send object enable #t))))))
+  (send f show #t))
