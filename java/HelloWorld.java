d6745f59d15561e514352b6de27b518bab1d240c
Eric Hanchrow <eric.hanchrow@gmail.com>
2004-07-28 23:27:55 +0000

    Golly ... Ah kin cawl C from Java

---
+++ java/HelloWorld.java
@@ -0,0 +1,11 @@
+class HelloWorld {
+    public native void displayHelloWorld();
+
+    static {
+        System.loadLibrary("hello");
+    }
+    
+    public static void main(String[] args) {
+        new HelloWorld().displayHelloWorld();
+    }
+}
