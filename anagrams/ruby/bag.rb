c47fd05c6a82bf0fff79e3f8dfbf77f20e79c1b3
Eric Hanchrow <eric.hanchrow@gmail.com>
2005-02-05 19:48:26 +0000

    *** empty log message ***

--- anagrams/ruby/bag.rb
+++ anagrams/ruby/bag.rb
@@ -2,22 +2,40 @@
 
 require 'test/unit'
 
+$primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101]
+
 class Bag
   def initialize (str)
-    @str = str.split(//).sort().join()
+    @product = 1
+    str.downcase().each_byte {
+      |b|
+      if(b >= ?a and b <= ?z)
+        index = b - ?a
+        @product *= $primes[index]
+      end
+    }
   end
   def empty
-    0 == @str.size()
+    1 == @product
   end
   def ==(other)
-    @str == other.str
+    @product == other.product
   end
   def -(other)
-    Bag.new("a")
+    if(@product % other.product != 0)
+      nil
+    else
+      p = Bag.new("")
+      p.product = @product / other.product
+      p
+    end
   end
   protected
-  def str
-    @str
+  def product
+    @product
+  end
+  def product=(x)
+    @product = x
   end
 end
 
@@ -31,5 +49,23 @@ class Bag_Tests < Test::Unit::TestCase
                  Bag.new("ab") - Bag.new("b"))
     assert_equal(Bag.new("b"),
                  Bag.new("ab") - Bag.new("a"))
+    assert((not( Bag.new("a") - Bag.new("b"))))
+    assert((not( Bag.new("a") - Bag.new("aa"))))
+    silly_long_string = "When first I was a wee, wee lad\n\
+Eating on my horse\n\
+I had to take a farting duck\n\
+Much to my remorse.\n\
+Oh Sally can't you hear my plea\n\
+When Roe V Wade is nigh\n\
+And candles slide the misty morn\n\
+With dimples on your tie."
+
+    ever_so_slightly_longer_string = silly_long_string + "x"
+    assert_equal(Bag.new("x"),
+                 Bag.new(ever_so_slightly_longer_string) - Bag.new(silly_long_string))
+    
+    assert_equal(Bag.new("abc"),
+                 Bag.new("ABC"), "properly insensitive to case")
+    
   end
 end
