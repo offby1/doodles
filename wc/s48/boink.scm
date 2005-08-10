7e9e11122c1a447a8f536a054b8f73f59970bb18
Eric Hanchrow <eric.hanchrow@gmail.com>
2005-08-10 13:40:49 +0000

    Save a list of dictionary words in the image.

--- wc/s48/boink.scm
+++ wc/s48/boink.scm
@@ -1,7 +1,10 @@
 ;;   -*- mode: scheme48; scheme48-package: boink -*-
 (define (boink)
-  (display "Boink!!")
+  (display "Boink!!") (newline)
   (let ((table '()))
+    (display "List length: ")
+    (display (length *the-list*))
+    (newline)
     (table-walk (lambda (k v)
                   (set! table (cons (cons k v) table))) *the-hash-table*)
     (write table)
@@ -10,6 +13,8 @@
 
 (define *the-hash-table*
   (make-string-table))
+
+(define *the-list* '())
 
 (define (read-line port)
   (iterate loop ((input* c port read-char))
@@ -21,12 +26,24 @@
         (eof-object)                   ; from the PRIMITIVES structure
       (list->string (reverse chars)))))
 
-(call-with-input-file
-    "ten-words"
-  (lambda (p)
-    (let loop ((c (read-char p)))
-      (if (not (eof-object? c))
-          (let* ((key (string c))
-                 (probe (table-ref *the-hash-table* key)))
-            (table-set! *the-hash-table* key (if probe (+ 1 probe) 1))
-            (loop (read-char p)))))))
+(if #t
+    (set! *the-list*
+          (call-with-input-file
+              "/usr/share/dict/words"
+            (lambda (p)
+              (let loop ((word (read-line p))
+                         (result '()))
+                (if (eof-object? word)
+                    result
+                  (loop (read-line p)
+                        (cons word result))
+                  )))))
+  (call-with-input-file
+      "/usr/share/dict/words"
+    (lambda (p)
+      (let loop ((c (read-char p)))
+        (if (not (eof-object? c))
+            (let* ((key (string c))
+                   (probe (table-ref *the-hash-table* key)))
+              (table-set! *the-hash-table* key (if probe (+ 1 probe) 1))
+              (loop (read-char p))))))))
