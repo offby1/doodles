1a377b93a50af4d25a89d136ac895157a778ca38
Eric Hanchrow <eric.hanchrow@gmail.com>
2005-12-06 06:59:08 +0000

    might work -- but it's so slow that I can't yet tell :-|

---
+++ anagrams/scheme/chicken/dict.scm
@@ -0,0 +1,47 @@
+(declare (unit dict))
+(declare (uses bag))
+
+(require-extension regex)
+
+(define word-acceptable?
+  (let ((has-vowel-regexp (regexp "[aeiou]" #t))
+        (has-non-ASCII-regexp (regexp "[^a-zA-Z]" #t)))
+    (lambda (word)
+      (let ((l (string-length word)))
+        (and (not (zero? l))
+             
+             ;; it's gotta have a vowel.
+             (string-match has-vowel-regexp word)
+             
+             ;; it's gotta be all ASCII, all the time.
+             (not (string-match has-non-ASCII-regexp word))
+             
+             ;; it's gotta be two letters long, unless it's `i' or `a'.
+             (or (string=? "i" word)
+                 (string=? "a" word)
+                 (< 1 l)))))))
+
+;; return a dictionary of words that can be made from CRITERION-BAG.
+;; The dictionary is a list of entries; each entry is (cons key words)
+(define (snarf-dictionary criterion-bag)
+  (let ((*the-hash-table* (make-hash-table =))
+        (rv '()))
+    (call-with-input-file
+        "words"
+      (lambda (p)
+        (display "Reading dictionary ... ") (flush-output)
+        (let loop ((word (read-line p)))
+          (if (eof-object? word)
+              (begin
+                (display "done")
+                (newline)
+                (hash-table->alist *the-hash-table*))
+            (begin
+              (if (word-acceptable? word)
+                  (let* ((num  (bag word))
+                         (prev (hash-table-ref *the-hash-table* num (lambda () #f))))
+                    (if (not prev)
+                        (set! prev '()))
+                    (set! prev (cons word prev))
+                    (hash-table-set! *the-hash-table* num prev)))
+              (loop (read-line p)))))))))
