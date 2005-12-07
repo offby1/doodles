82594274d990ad36852bb2684781347548e0f8f9
Eric Hanchrow <eric.hanchrow@gmail.com>
2005-12-07 05:04:41 +0000

    * anagrams.lisp(anagrams): takes new optional limit

--- anagrams/cl/anagrams.lisp
+++ anagrams/cl/anagrams.lisp
@@ -10,19 +10,17 @@
                       more-anagrams))
           these-words))
 
-(defmacro maybe-dump (ans)
-  `(if (zerop depth)
-       (dolist (a ,ans) (format *error-output* "~a~%" a))))
-
 (defmacro while (test &rest body)
   `(do ()
        ((not ,test))
      ,@body))
 
-(defun anagrams-internal (bag dict depth)
-  
-  (let ((rv ()))
-    (while (not (null dict))
+(defun anagrams-internal (bag dict limit)
+  (let ((rv ())
+        (length 0))
+    (while (and (or (not limit)
+                    (> limit length))
+                (not (null dict)))
       (let* ((entry (car dict))
              (this-bag (car entry))
              (these-words (cdr entry))
@@ -30,22 +28,20 @@
         (when smaller-bag
           (if (bag-emptyp smaller-bag)
               (let ((combined (mapcar #'list these-words)))
-                ;(maybe-dump combined)
-                (setf rv (nconc rv combined)))
-              (let ((more-anagrams (anagrams-internal smaller-bag dict (+ 1 depth))))
-
+                (setf rv (nconc rv combined))
+                (incf length (length combined)))
+              (let ((more-anagrams (anagrams-internal smaller-bag dict limit)))
                 (when more-anagrams
                   (let ((combined (combine these-words more-anagrams)))
-                    ;(maybe-dump combined)
-                    (setf rv (nconc rv combined))))))))
+                    (setf rv (nconc rv combined))
+                    (incf length (length combined))))))))
       (setf dict (cdr dict)))
-
     rv))
 
-(defun anagrams (string)
+(defun anagrams (string &optional limit)
   (let ((b  (bag string)))
     (init b)
-    (let ((result (anagrams-internal b *dict* 0)))
+    (let ((result (anagrams-internal b *dict* limit)))
       (prog1 result
         (format *error-output* ";; ~a anagrams of ~a~%" (length result)
                 string)))))
