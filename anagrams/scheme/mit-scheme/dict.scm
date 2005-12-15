5bae4b762bccfa1baf6d7f3bc645e66b4e5b0c14
Eric Hanchrow <eric.hanchrow@gmail.com>
2005-12-15 01:34:26 +0000

    Not sure it's working or not.

--- anagrams/scheme/mit-scheme/dict.scm
+++ anagrams/scheme/mit-scheme/dict.scm
@@ -2,9 +2,10 @@
 (load-option 'rb-tree)
 (define word-acceptable?
   (let ((has-vowel-regexp (rexp-compile (rexp-case-fold (string->char-set "aeiou"))))
-        (has-non-ASCII-regexp (rexp-compile (char-set-invert  (ascii-range->char-set
-                                                               (char->ascii #\a)
-                                                               (+ 1 (char->ascii #\z)))))))
+        (has-non-letter-regexp (rexp-compile (rexp-case-fold (char-set-invert
+                                                             (char-set-union
+                                                              (ascii-range->char-set (char->ascii #\A) (+ 1 (char->ascii #\Z)))
+                                                              (ascii-range->char-set (char->ascii #\a) (+ 1 (char->ascii #\z)))))))))
     (lambda (word)
       (let ((l (string-length word)))
         (and (not (zero? l))
@@ -12,8 +13,8 @@
              ;; it's gotta have a vowel.
              (re-string-search-forward has-vowel-regexp word)
              
-             ;; it's gotta be all ASCII, all the time.
-             (not (re-string-search-forward has-non-ASCII-regexp word))
+             ;; it's gotta be all letters
+             (not (re-string-search-forward has-non-letter-regexp word))
              
              ;; it's gotta be two letters long, unless it's `i' or `a'.
              (or (string=? "i" word)
@@ -42,17 +43,18 @@
         (let loop ((word (read-line p))
                    (words-read 0))
           (if (eof-object? word)
-              (let ((al (hash-table->alist *the-table*)))
-                (write (car al))
+              (let ((pruned (keep-matching-items  (hash-table->alist *the-table*)
+                                                  (lambda (entry)
+                                                    (subtract-bags criterion-bag (car entry))))))
+                (display words-read)
+                (display " words in dictionary; ")
+                (display "kept ")
+                (display (length pruned))
+                (display " of them")
                 (newline)
-                (keep-matching-items al 
-                                     (lambda (p)
-                                       (let ((number (car p) )
-                                             (words (cadr p)))
-                                         (subtract-bags criterion-bag number)))))
+                pruned)
             (begin
-              (if (and (zero? (remainder words-read 1))
-                       (word-acceptable? word))
+              (if (word-acceptable? word)
                   (let* ((num  (bag word))
                          (prev (hash-table/get *the-table* num '())))
                     (set! prev (cons word prev))
