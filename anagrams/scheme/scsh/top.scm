c312a8102b49865a0361e93b4bb2679658fd2f66
Eric Hanchrow <eric.hanchrow@gmail.com>
2006-03-01 00:45:00 +0000

    Not yet working.

--- anagrams/scheme/scsh/top.scm
+++ anagrams/scheme/scsh/top.scm
@@ -1,10 +1,8 @@
-#!/usr/local/bin/scsh \
--o scheme-with-scsh -o tables -o primitives -o srfi-23 -o srfi-1 -o sort -l bag.scm -l dict.scm -e main -s
+#!/usr/local/bin/scshvm -o scshvm -h 28000000 -i /usr/lib/scsh-0.6/scsh.image \
+-o scheme-with-scsh -o tables -o primitives -o srfi-23 -o srfi-1 -o sort -l bag.scm -l dict.scm -l anagrams.scm -e main -s
 !#
 
 (define (main args)
-  (define b (bag "cat dog"))
-  (define t (snarf-dictionary b))
-  (display t)
+  (display (anagrams "shephard"))
   (newline)
   )
