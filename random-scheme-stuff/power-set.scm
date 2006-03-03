0388f3d1eca0454ddc665987b6a6da4d76c5a0a8
Eric Hanchrow <eric.hanchrow@gmail.com>
2006-03-03 21:31:32 +0000

    no need to include srfi-1

--- random-scheme-stuff/power-set.scm
+++ random-scheme-stuff/power-set.scm
@@ -3,18 +3,15 @@
 ;; (foo bar) => ((foo bar) (foo) (bar) ())
 ;; (f b z) => ((f b z) (f b) (f z) (b z) (f) (b) (z) ())
 
-;; for mzscheme v300
-
-(require (lib "1.ss" "srfi"))
-
-(define (combine sym ps-list)
-  (append
-   (map (lambda (sym-seq)
-          (cons sym sym-seq))
-        ps-list)
-   ps-list))
-
 (define (power-set seq)
+
+  (define (combine sym ps-list)
+    (append
+     (map (lambda (sym-seq)
+            (cons sym sym-seq))
+          ps-list)
+     ps-list))
+
   (if (null? seq)
       '(())
     (combine (car seq) (power-set (cdr seq))))
