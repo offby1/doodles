221b4977e94a672a3614d40aedbab22ab56c411f
Eric Hanchrow <eric.hanchrow@gmail.com>
2006-05-14 17:19:03 +0000

    *** empty log message ***

---
+++ random-scheme-stuff/memoize.scm
@@ -0,0 +1,22 @@
+(define memoize
+  (let ((cache (make-hash-table 'equal)))
+    (lambda (func)
+      (lambda args
+        (let  ((probe (hash-table-get cache args (lambda () #f))))
+          (or probe
+              (let ((new-value (apply func args)))
+                (hash-table-put! cache args new-value)
+                new-value)))))))
+
+(define fib
+  (memoize
+   (lambda (x)
+     (cond
+      ((zero? x)
+       0)
+      ((= 1 x)
+       1)
+      (else
+       (+
+        (fib (- x 1))
+        (fib (- x 2))))))))
