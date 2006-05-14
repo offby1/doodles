4f3a8ebfaf218154ac19ebe1ced3779d31dd69f6
Eric Hanchrow <eric.hanchrow@gmail.com>
2006-05-14 17:31:30 +0000

    *** empty log message ***

--- random-scheme-stuff/memoize.scm
+++ random-scheme-stuff/memoize.scm
@@ -1,3 +1,8 @@
+#! /bin/sh
+#| Hey Emacs, this is -*-scheme-*- code!
+exec mzscheme -qr "$0" ${1+"$@"}
+|#
+
 ;; for PLT scheme 301 (and probably earlier too)
 (define memoize
   (let ((cache (make-hash-table 'equal)))
@@ -12,6 +17,11 @@
              (printf "Stored ~a~%" new-value)
              new-value)))))))
 
+;; uncomment this to disable memoization, and see how much slower it
+;; is
+
+;;(set! memoize values)
+
 (define fib
   (memoize
    (lambda (x)
@@ -24,3 +34,6 @@
        (+
         (fib (- x 1))
         (fib (- x 2))))))))
+
+(let ((n 35))
+  (printf "Fib ~a: ~a~%" n (fib n)))
