006892ca1f9c86a8a8a6a40b95b2c58eba7d3b14
Eric Hanchrow <eric.hanchrow@gmail.com>
2006-05-15 19:38:21 +0000

    reimplementation of the Jefferson Wheel cipher.  Might be amusing to compare it to how I did it originally, lo those many years ago.

---
+++ crypt/jw2.scm
@@ -0,0 +1,127 @@
+(require (only (lib "1.ss" "srfi") filter iota)
+         (lib "trace.ss"))
+
+;; #\A => 0
+;; #\a => 0
+;; #\z => 25 etc
+(define (letter->integer c)
+  (unless (char-alphabetic? c)
+    (raise-type-error 'letter->integer "alphabetic" c))
+  (- (char->integer (char-downcase c)) (char->integer #\a)))
+
+;; inverse of the lower-case part of the above
+(define (integer->letter i)
+  (unless (and (exact? i)
+               (not (negative? i))
+               (< i *alphabet-length*))
+    (raise-type-error 'integer->letter (format "exact, non-negative integer less than ~a" *alphabet-length*) i))
+  (integer->char (+ i (char->integer #\a))))
+
+(define *alphabet-length* 26)
+
+;; shuffles in-place.  Destructive.  Got it?
+(define (fisher-yates-shuffle! v)
+  (define (swap! i1 i2)
+    (let ((tmp (vector-ref v i1)))
+      (vector-set! v i1 (vector-ref v i2))
+      (vector-set! v i2 tmp)))
+  (let ((l (vector-length v)))
+    (do ((top-index (sub1 l) (sub1 top-index)))
+        ((zero? top-index) v)
+      (let ((bottom-index (random top-index)))
+        (swap! bottom-index top-index))))
+  v)
+
+(define (find-index object vector)
+  (let loop ((slots-to-examine (vector-length vector)))
+    (if (positive? slots-to-examine)
+        (let* ((this-index (- slots-to-examine 1))
+               (this-item (vector-ref vector this-index)))
+          (if (eq? this-item object)
+              this-index
+            (loop (- slots-to-examine 1))))
+      (error "Couldn't find" object 'in vector))))
+
+(define (make-one-wheel)
+  (let ((permutation (
+                      ;;values
+                      fisher-yates-shuffle!
+                      (list->vector (iota *alphabet-length*)))))
+    (cons
+     ;; encrypt
+     (lambda (c offset)
+       (let ((result (integer->letter (vector-ref permutation (modulo (+ offset (letter->integer c)) *alphabet-length*)))))
+
+         result))
+     ;; decrypt
+     (lambda (c offset)
+       (let* ((index (find-index (letter->integer c) permutation))
+              (result (integer->letter (modulo (- index offset) *alphabet-length*))))
+
+         result)))))
+
+(define (lc-letters-only string)
+  (map char-downcase (filter char-alphabetic? (string->list string))))
+
+(define (string->integers string)
+  (map letter->integer (lc-letters-only string)))
+
+(define (jw input)
+  (map (lambda ignored (make-one-wheel)) (string->integers input)))
+
+(define (random-offset)
+   (random *alphabet-length*)
+)
+
+(define (general-purpose input-string spindle encrypting? offset)
+  (let ((original-spindle spindle))
+    (define (loop letters spindle offset result)
+      (cond
+       ((null? letters)
+        (list->string (reverse result)))
+       ((null? spindle)
+        (loop letters
+              original-spindle
+              (random-offset)
+              result))
+       (else
+        (let ((this-wheel (car spindle)))
+          (loop (cdr letters)
+                (cdr spindle)
+                offset
+                (cons ((
+                        (if encrypting? car cdr)
+                        (car spindle)) (car letters) offset) result))))))
+
+    (loop
+     (lc-letters-only input-string)
+     original-spindle
+     offset
+     '())))
+
+;; if offset is not #f, then encrypt, using a that offset.
+;; otherwise, decrypt by returning possible offsets.
+(define (hmm input-string spindle offset)
+  (unless (or (not offset)
+              (and (integer? offset)
+                   (exact? offset )
+                   (not (negative? offset))))
+    (raise-type-error 'hmm "#f, or exact non-negative integer" offset))
+  (if offset
+      (general-purpose input-string spindle #t offset)
+    (map (lambda (offset)
+           (general-purpose input-string spindle #f offset))
+         (iota *alphabet-length*))))
+
+(define (encrypt input spindle)
+  (hmm input spindle (random-offset)))
+
+(define (decrypt input spindle)
+  (hmm input spindle #f))
+
+(define input "Hello, world!!")
+(define spindle (jw input))
+(define cipher (encrypt  input spindle))
+(printf "~s encrypts to ~s~%" input cipher)
+(define recovered (decrypt cipher spindle))
+(printf "~s decrypts to ~s~%" cipher recovered)
