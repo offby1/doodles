5f5425a67929c622858fd3027d593402db0e03f8
Eric Hanchrow <eric.hanchrow@gmail.com>
2006-06-26 09:12:49 +0000

    comments and tidying

--- random-scheme-stuff/t2.scm
+++ random-scheme-stuff/t2.scm
@@ -3,19 +3,30 @@
 exec mzscheme -qu "$0" ${1+"$@"}
 |#
 
+;; given a bunch of items of random sizes, and a bunch of identical
+;; empty containers, put the items into the containers in such a way
+;; as to fill each container as full as each other (as close as
+;; possible).
+
+;; Do this by sorting the items in descending order by size, then for
+;; each item, add it to the emptiest container.
+
+;; see "triumph.scm" for a longer explanation of why this is
+;; interesting.  That file says that it took me _hours_ to write it,
+;; which is appalling; this here took maybe half an hour.  Perhaps I'm
+;; a better programmer now :-|.
 (module t2 mzscheme
 (require (only (lib "1.ss" "srfi") fold iota)
          (only (lib "13.ss" "srfi") string-join)
          (only (lib "list.ss") quicksort)
          "normals.ss")
 
-(print-struct #t)
 (define-struct item (name size) #f)
 (define-struct container (items remaining-capacity) #f)
 
-(define *containers* (map (lambda (ignored)
+(define *containers* (map (lambda ignored
                             (make-container '() 0))
-                          (iota 5)))
+                          (iota 8)))
 
 (define (emptiest containers)
   (fold
@@ -28,15 +39,18 @@ exec mzscheme -qu "$0" ${1+"$@"}
 
 (define (container-add! c i)
   (set-container-items! c (cons i (container-items c)))
-  (set-container-remaining-capacity! c (-
-                                        (container-remaining-capacity
-                                         c)
-                                        (item-size i))))
+  (set-container-remaining-capacity!
+   c
+   (-
+    (container-remaining-capacity c)
+    (item-size i))))
+
 (define *random-items*
   (map (lambda (n)
          (make-item (number->string n) (inexact->exact (round (* (one-unit-normal) 100)))))
-       (iota 50)))
+       (iota 3000)))
 
+;; pack the items into the containers.
 (for-each (lambda (i)
             (let ((c (emptiest *containers*)))
               (container-add! c i)))
@@ -45,16 +59,22 @@ exec mzscheme -qu "$0" ${1+"$@"}
            (lambda (i1 i2)
              (> (item-size i1)
                 (item-size i2)))))
+
 ;(printf "~a~%" (map item-size *random-items*))
+
+;; now show the packed containers.
 (for-each (lambda (c)
             (let ((sizes (map item-size (container-items c))))
-            (printf "~a = ~a~%"
-                    (apply + sizes)
-                    (string-join
-                     (map number->string (reverse sizes))
-                     " + "
-                     'infix))
-            (newline)))
-           *containers*)
+              (printf "~a = ~a~%"
+                      (apply + sizes)
+                      (string-join
+                       (map number->string (reverse sizes))
+                       " + "
+                       'infix))))
+          (quicksort
+           *containers*
+           (lambda (c1 c2)
+             (> (container-remaining-capacity c1)
+                (container-remaining-capacity c2)))))
 (newline)
 )
