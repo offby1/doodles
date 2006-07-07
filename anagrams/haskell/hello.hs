560096d815e49846fcea93b71378ddba2de0ec54
Eric Hanchrow <eric.hanchrow@gmail.com>
2006-07-07 15:09:02 +0000

    *** empty log message ***

--- anagrams/haskell/hello.hs
+++ anagrams/haskell/hello.hs
@@ -1,8 +1,12 @@
+-- -*-haskell-*-
+
 module Main where
 
 fact :: Integer -> Integer
-fact x = if x == 0 then 1 else x * fact (x - 1)
+fact x = case x of
+           0 -> 1
+           x -> x * fact (x - 1)
 
 main = do 
-     print "Hello, world "
-     print (fact (100))
+  print "Hello, world "
+  print (fact (100))
