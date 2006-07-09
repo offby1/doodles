b843ee42cfa442e32cc3204c869cf0405af0d153
Eric Hanchrow <eric.hanchrow@gmail.com>
2006-07-09 19:22:45 +0000

    much terser

--- anagrams/haskell/bag.hs
+++ anagrams/haskell/bag.hs
@@ -1,43 +1,24 @@
 -- -*-haskell-*-
 
-module Bag where
+module Bag (make_bag) where
 import Char
 
 make_bag :: String -> Integer
 make_bag []     = 1
 make_bag (c:cs) = char_prime (c) * make_bag cs
 
+-- thanks to "Cale" (Cale Gibbard
+-- (i=foobar@London-HSE-ppp3543475.sympatico.ca)) for "primes"
+primes = sieve [2..]; sieve (x:xs) = x : sieve [y | y <- xs, y `mod` x /= 0]
+
 char_prime :: Char -> Integer
-char_prime c = case toLower (c) of
-           'a' -> 2
-           'b' -> 3
-           'c' -> 5
-           'd' -> 7
-           'e' -> 11
-           'f' -> 13
-           'g' -> 17
-           'h' -> 19
-           'i' -> 23
-           'j' -> 29
-           'k' -> 31
-           'l' -> 37
-           'm' -> 41
-           'n' -> 43
-           'o' -> 47
-           'p' -> 53
-           'q' -> 59
-           'r' -> 61
-           's' -> 67
-           't' -> 71
-           'u' -> 73
-           'v' -> 79
-           'w' -> 83
-           'x' -> 89
-           'y' -> 97
-           'z' -> 101
-           _-> 1
+char_prime c | (lc >= 'a') && (lc <= 'z') = primes !! (ord (lc) - ord('a'))
+             | True = 1
+             where lc = toLower (c)
 
 -- to test this, start ghci, and type
 -- :load bag.hs
 --  print "Hello, world "
 --  print (make_bag ("Hello, world"))
+
+-- that last should print 828806486967613
