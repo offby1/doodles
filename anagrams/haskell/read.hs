6dc6d0a0fd1777a5fc684b49d042295ebb2c2ea2
Eric Hanchrow <eric.hanchrow@gmail.com>
2006-07-11 14:43:24 +0000

    *** empty log message ***

--- anagrams/haskell/read.hs
+++ anagrams/haskell/read.hs
@@ -2,9 +2,9 @@ module Main where
 import Bag
 import qualified Data.Map as Map
 
-flubber :: [String] -> [(Integer, String)]
+flubber :: [String] -> Map.Map Integer String
 flubber lines =
-        map (\w -> (make_bag (w), w)) lines
+        Map.fromList (map (\w -> (make_bag (w), w)) lines)
 
 --prepend :: a -> [a] -> [a]
 prepend item [] = [item]
@@ -14,5 +14,7 @@ prepend item [xs]
 
 main= do
       x <- readFile ("words")
-      let dict = Map.fromList (flubber (lines (x)))
-          in print (dict Map.! 710)
+      let dict = flubber (lines (x))
+          in do
+             print (dict);
+             print (dict Map.! 710)
