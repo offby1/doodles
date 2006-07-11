259bb0e21306c2e2faf59fd16d2eb9762635245d
Eric Hanchrow <eric.hanchrow@gmail.com>
2006-07-11 16:39:06 +0000

    closer (maybe) but still not quite

--- anagrams/haskell/read.hs
+++ anagrams/haskell/read.hs
@@ -15,15 +15,25 @@ from_strings [] = M.empty
 from_strings (line:lines) =
              adjoin (make_bag (line)) line (from_strings (lines))
 
+isVowel :: Char -> Bool
+isVowel c = 
+        any (\v -> v == c) "aeiou" 
+
+hasVowel :: String -> Bool
+hasVowel s =
+         any isVowel s
+
 acceptable :: String -> Bool
 acceptable word =
-           all isAlpha word && ((length (word) > 1)
-                                || word == "a"
-                                || word == "i")          
+           all isAlpha word
+            && hasVowel word
+            && ((length (word) > 1)
+               || word == "a"
+               || word == "i")          
 
 main= do
       x <- readFile ("/usr/share/dict/words")
-      let dict = from_strings (map (map toLower) (filter acceptable (lines x)))
+      let dict = from_strings (filter acceptable (map (map toLower) (lines x)))
           in do
              print (sum (map length (M.elems dict)))
              print (take 3 (M.toList dict));
