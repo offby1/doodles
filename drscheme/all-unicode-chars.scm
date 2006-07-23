bb1d5cfb876122d6f6be6ad2f93411c92e03ad52
Eric Hanchrow <eric.hanchrow@gmail.com>
2006-07-23 16:28:23 +0000

    much simpler

--- drscheme/all-unicode-chars.scm
+++ drscheme/all-unicode-chars.scm
@@ -1,13 +1,14 @@
+#! /bin/sh
+#| Hey Emacs, this is -*-scheme-*- code!
+exec mzscheme -qu "$0" ${1+"$@"}
+|#
+
 ;;; unicode chars range from 0 through #x10FFFF inclusive, except for
 ;;; #xd800 through #xdfff inclusive.
 
 (module all-unicode-chars mzscheme
-  (display
-   (let ((s (make-string (+ 1 #x10FFFF))))
-     (let loop ((slots-filled 0))
-       (if (= slots-filled (string-length s))
-           s
-         (begin
-           (unless (<= #xd800 slots-filled #xdfff)
-             (string-set! s slots-filled (integer->char slots-filled)))
-           (loop (+ 1 slots-filled))))))))
+(let loop ((chars-considered 0))
+  (when (< chars-considered #x110000)
+    (unless (<= #xd800 chars-considered #xdfff)
+      (display (integer->char chars-considered)))
+    (loop (+ 1 chars-considered)))))
