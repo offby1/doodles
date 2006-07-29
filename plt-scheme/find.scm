bf2571a10c2dc8d858b9d9d3231437c1d49044db
Eric Hanchrow <eric.hanchrow@gmail.com>
2006-07-29 16:53:58 +0000

    *** empty log message ***

--- plt-scheme/find.scm
+++ plt-scheme/find.scm
@@ -2,25 +2,29 @@
 (define example `(".bashrc" ,(make-directory "Itineraries" '("mexico" "new york")) "Phones"))
 (define output `("./.bashrc" "./Itineraries" "./Itineraries/mexico" "./Itineraries/new york" "./Phones"))
 
+(define (pfx d f)
+  (string-append d "/" f))
+
+(define (mappfx d seq)
+  (map (lambda (entry)
+         (pfx  d entry))
+        seq))
+
 (define (dir->list d)
   (cons (directory-name d)
-        (map (lambda (entry)
-               (string-append (directory-name d) "/" entry))
-             (directory-entries d))))
+        (mappfx (directory-name d) (directory-entries d))))
 
 (require (lib "trace.ss"))
 (define (flatten current-dir thing)
   (cond
    ((string? thing)
-    (list (string-append current-dir "/" thing)))
+    (list (pfx  current-dir thing)))
    ((list? thing)
     (apply append (map (lambda (entry)
            (flatten current-dir entry))
          thing)))
    ((directory? thing)
-    (map (lambda (entry)
-           (string-append current-dir "/" entry))
-         (dir->list thing)))
+    (mappfx  current-dir (dir->list thing)))
    (#t
     (error "Ain't no thang" thing))))
 (trace flatten)
