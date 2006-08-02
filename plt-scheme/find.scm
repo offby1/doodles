1088bff3cc35599221da101d7bf1eb54eee2abbf
Eric Hanchrow <eric.hanchrow@gmail.com>
2006-08-02 13:55:17 +0000

    *** empty log message ***

--- plt-scheme/find.scm
+++ plt-scheme/find.scm
@@ -1,6 +1,11 @@
 (module find mzscheme
 (require (lib "file.ss")
-         (lib "1.ss" "srfi"))
+         (only (lib "1.ss" "srfi") unfold))
+
+(define (tail path)
+  (let-values (((base name must-be-dir?)
+                (split-path path)))
+    name))
 
 (define (sans-tail path)
   (let-values (((base name must-be-dir?)
@@ -23,10 +28,16 @@
   (member (build-path ".svn")
           (path->components path)))
 
-(fold-files
- (lambda (path flavor seeds)
-   (if (is-svn? path)
-       seeds
-     (cons path seeds)))
- '())
+(define (dn thing)
+  (display thing)
+  (newline))
+
+(for-each dn
+          (fold-files
+           (lambda (path flavor seeds)
+             (fprintf (current-error-port) "Considering ~s~%" path)
+             (if (is-svn? path)
+                 seeds
+               (cons path seeds)))
+           '()))
 )
