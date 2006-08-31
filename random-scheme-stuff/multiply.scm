ddf6cbf62a2a6b5aa510e66056c3ae3e3f87e565
Eric Hanchrow <eric.hanchrow@gmail.com>
2006-08-31 21:31:59 +0000

    *** empty log message ***

--- random-scheme-stuff/multiply.scm
+++ random-scheme-stuff/multiply.scm
@@ -5,25 +5,25 @@
 ;; representing a car, and a number representing a distance.  And say
 ;; you have many drivers, many cars, and many distances, and you want
 ;; to call the function for each possible combination.  E.g:
-;; 
+;;
 ;; (define drive
 ;;   (lambda (drivers-name
 ;;            car
 ;;            distance)
 ;;     (string-append drivers-name " drives " (symbol->string car) " " (number->string distance) " miles")))
-;; 
+;;
 ;; (define drivers   (list "Sarah" "Tom" "Zippy"))
 ;; (define cars      (list 'Camaro 'Beat-up-old-Honda 'station-wagon))
 ;; (define distances (list 3 5 100))
-;; 
+;;
 ;; If you do
-;; 
+;;
 ;; (map (lambda (args)
 ;;        (apply drive args))
 ;;      (multiply drivers cars distances))
-;; 
+;;
 ;; you'll get
-;; 
+;;
 ;; ("Sarah drives camaro 3 miles"
 ;;  "Sarah drives camaro 5 miles"
 ;;  "Sarah drives camaro 100 miles"
@@ -51,39 +51,20 @@
 ;;  "Zippy drives station-wagon 3 miles"
 ;;  "Zippy drives station-wagon 5 miles"
 ;;  "Zippy drives station-wagon 100 miles")
-;; 
+;;
 ;; I think you could also describe this function as returning the
 ;; `Cartesian Product' of its arguments, if you consider the arguments
 ;; as defining `domain's.
 
-(define multiply
-  (lambda args
+;; from kilimanjaro on #scheme
+(define (multiply list-of-lists)
+  (define (foobar lst lsts)
+    (if (null? lsts)
+        (map list lst)
+      (apply append (map (lambda (elt)
+                           (map (lambda (x) (cons elt x)) lsts))
+                         lst))))
 
-    (define (distribute-1 atom lst)
-
-      (if (null? lst)
-          lst
-        (map (lambda (elt)
-               (cons atom elt))
-             lst)))
-
-    (define (distribute-3 lst lst-o-lst)
-    
-      (if (null? lst)
-          '()
-        (append
-         (distribute-1 (car lst)
-                       lst-o-lst)
-         (distribute-3 (cdr lst)
-                       lst-o-lst))))
-
-    (cond
-     ((null? args)
-      '())
-     ((= 1 (length args))
-      (map list (car args)))
-     (#t 
-      (distribute-3 (car args) (apply multiply (cdr
-                                                args)))))))
-
-(provide 'multiply)
+  (if (null? list-of-lists)
+      '()
+    (foobar (car list-of-lists) (cartesian-product (cdr list-of-lists)))))
