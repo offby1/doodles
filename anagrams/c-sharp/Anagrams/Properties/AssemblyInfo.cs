bbdc7375303eb817f3b78a4de518f6d649f37e4e
Eric Hanchrow <eric.hanchrow@gmail.com>
2007-03-29 08:02:00 +0000

    Big scary reorganization: renamed last vestigal WindowsApplication1 to Anagrams

--- anagrams/c-sharp/Anagrams/Properties/AssemblyInfo.cs
+++ anagrams/c-sharp/Anagrams/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WindowsApplication1")]
+[assembly: AssemblyTitle("Anagrams")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WindowsApplication1")]
+[assembly: AssemblyProduct("Anagrams")]
 [assembly: AssemblyCopyright("Copyright ©  2007")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
