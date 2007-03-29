9555be44938d018b2dec5ea1470257f7852296b9
Eric Hanchrow <eric.hanchrow@gmail.com>
2007-03-29 20:33:22 +0000

    Tiny step towards subtraction

--- anagrams/c-sharp/Anagrams/Bag.cs
+++ anagrams/c-sharp/Anagrams/Bag.cs
@@ -4,23 +4,34 @@ using System.Text;
 
 namespace Anagrams
 {
-
     class Bag
     {
+        static private string subtract_strings(string minuend, string subtrahend)
+        {
+            Bag m = new Bag(minuend);
+            Bag s = new Bag(subtrahend);
+            return m.subtract(s).toString();
+        }
+
         private string guts;
         public Bag(string s)
         {
-            Char [] chars = s.ToLower(). ToCharArray();
+            Char[] chars = s.ToLower().ToCharArray();
             Array.Sort(chars);
-            Char [] letters = Array.FindAll<char>(chars, Char.IsLetter);
+            Char[] letters = Array.FindAll<char>(chars, Char.IsLetter);
             System.Text.StringBuilder sb = new System.Text.StringBuilder();
             sb.Insert(0, letters);
             guts = sb.ToString();
         }
-
-        public static void test ()
+        public Bag subtract(Bag subtrahend)
         {
-            Console.WriteLine ("Pretend the bag tests all passed.");
+            return new Bag("");
+        }
+        public static void test()
+        {
+            string diff = subtract_strings("dog", "god");
+            if (diff != "") throw new Exception("Uh oh");
+            Console.WriteLine("Pretend the bag tests all passed.");
         }
 
         public string toString()
