b151a1e73140d270ad4536ecc548e7304036c278
Eric Hanchrow <eric.hanchrow@gmail.com>
2007-05-31 01:57:38 +0000

    already we're discovering some truths!

--- autogram/s3.scm
+++ autogram/s3.scm
@@ -8,9 +8,10 @@ exec mzscheme -M errortrace -qu "$0" ${1+"$@"}
 (module s3 mzscheme
 
 (require
- (only (lib "1.ss" "srfi") fold)
+ (only (lib "1.ss" "srfi")
+       filter
+       fold)
  (planet "numspell.ss" ("neil" "numspell.plt" 1 0))
- (planet "memoize.ss" ("dherman" "memoize.plt" 2 1))
  "counter.ss")
 
 (define a-template (list
@@ -20,28 +21,28 @@ exec mzscheme -M errortrace -qu "$0" ${1+"$@"}
                     (cons #\b 0)
 
                     ", " (cons #\c 0)
-                    ", " (cons #\d 0)
-                    ", " (cons #\e 0)
-                    ", " (cons #\f 0)
-                    ", " (cons #\g 0)
-                    ", " (cons #\h 0)
-                    ", " (cons #\i 0)
-                    ", " (cons #\j 0)
-                    ", " (cons #\k 0)
-                    ", " (cons #\l 0)
-                    ", " (cons #\m 0)
-                    ", " (cons #\n 0)
-                    ", " (cons #\o 0)
-                    ", " (cons #\p 0)
-                    ", " (cons #\q 0)
-                    ", " (cons #\r 0)
-                    ", " (cons #\s 0)
-                    ", " (cons #\t 0)
-                    ", " (cons #\u 0)
-                    ", " (cons #\v 0)
-                    ", " (cons #\w 0)
-                    ", " (cons #\x 0)
-                    ", " (cons #\y 0)
+;;                     ", " (cons #\d 0)
+                     ", " (cons #\e 0)
+;;                     ", " (cons #\f 0)
+;;                     ", " (cons #\g 0)
+;;                     ", " (cons #\h 0)
+;;                     ", " (cons #\i 0)
+;;                     ", " (cons #\j 0)
+;;                     ", " (cons #\k 0)
+;;                     ", " (cons #\l 0)
+;;                     ", " (cons #\m 0)
+;;                     ", " (cons #\n 0)
+;;                     ", " (cons #\o 0)
+;;                     ", " (cons #\p 0)
+;;                     ", " (cons #\q 0)
+;;                     ", " (cons #\r 0)
+;;                     ", " (cons #\s 0)
+;;                     ", " (cons #\t 0)
+;;                     ", " (cons #\u 0)
+;;                     ", " (cons #\v 0)
+;;                     ", " (cons #\w 0)
+;;                     ", " (cons #\x 0)
+;;                     ", " (cons #\y 0)
                     ", and " (cons #\z 0)
                     "."
                     ))
@@ -61,8 +62,9 @@ exec mzscheme -M errortrace -qu "$0" ${1+"$@"}
          '()
          t)))
 
-;; yes, memoizing really does make it faster.
-(define/memo* (template->counts t)
+;; memoization seems pointless, since if we're searching for truths,
+;; we should never call this twice on the same template.
+(define (template->counts t)
   (define (survey s)
     (let ((counts (make-count)))
       (let loop ((chars-examined 0))
@@ -99,10 +101,41 @@ exec mzscheme -M errortrace -qu "$0" ${1+"$@"}
          '()
          t)))
 
-(let loop ((x 10000)
-           (t a-template))
-  (when (positive? x)
-    ;;(printf "~a~%" (apply string-append (template->strings t)))
-    (loop (sub1 x)
-          (update-template t (template->counts t)))))
-)
+(define (just-the-conses seq)
+  (filter pair? seq))
+
+(define (make-calm-notifier thunk)
+  (define (is-power-of-two? x)
+    (or (= 1 x)
+        (and (not (odd? x))
+             (is-power-of-two? ( / x 2)))))
+  (let ((invocation-count 0))
+    (lambda ()
+      (set! invocation-count (add1 invocation-count))
+      (if (is-power-of-two? invocation-count)
+          (thunk)))))
+
+(let* ((seen (make-hash-table 'equal))
+       (fb (make-calm-notifier
+            (lambda ()
+              (fprintf
+               (current-error-port)
+               "Hash table has ~a entries.~%"
+               (hash-table-count seen))))))
+  (let loop ((x 10)
+             (t a-template))
+    (if (zero? x)
+        (printf "I give up.~%")
+      (let ((next (update-template t (template->counts t))))
+        ;; this assumes that templates will always have keys in the
+        ;; same order.
+        (if (equal? next t)
+            (printf "We got a winner: ~s~%" (apply string-append (template->strings t)))
+          (let ((key (just-the-conses t)))
+            (if (hash-table-get seen key #f)
+                (printf "Oh hell, we looped.~%")
+              (begin
+                (hash-table-put! seen key #t)
+                (fb)
+                (loop (sub1 x)
+                      next))))))))))
