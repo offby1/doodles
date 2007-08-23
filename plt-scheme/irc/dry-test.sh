1fd172ec67203d600315d0a3549c3c75d56bc3c9
Eric Hanchrow <eric.hanchrow@gmail.com>
2007-08-23 15:07:36 +0000

    ensure we get bash, not sh.  Oddly this script worked fine on my Ubuntu 7.04 ("Feisty Fawn") laptop earlier, but only now works on my Ubuntu 6.06.1 LTS ("Dapper Drake") desktop

--- plt-scheme/irc/dry-test.sh
+++ plt-scheme/irc/dry-test.sh
@@ -1,21 +1,21 @@
-#!/bin/sh
+#!/bin/bash -x
 
 (
 
-    echo  ":localhost. 001 rudybot :Welcome to the Debian dancer-ircd Network rudybot \\r"
-    echo  ":localhost 366 rudybot #scheme-bots :what's up, homes? \\r"
+    echo -e ":localhost. 001 rudybot :Welcome to the Debian dancer-ircd Network rudybot \\r"
+    echo -e ":localhost 366 rudybot #scheme-bots :what's up, homes? \\r"
 
-    echo ":a!b@c PRIVMSG #scheme-bots :hey buddy\\r"
-    echo ":a!b@c PRIVMSG #scheme-bots :rudybot: hey buddy\\r"
-    echo ":a!b@c PRIVMSG rudybot :hey buddy\\r"
-    echo ":a!b@c PRIVMSG #scheme-bots :rudybot: seen a?\\r"
+    echo -e ":a!b@c PRIVMSG #scheme-bots :hey buddy\\r"
+    echo -e ":a!b@c PRIVMSG #scheme-bots :rudybot: hey buddy\\r"
+    echo -e ":a!b@c PRIVMSG rudybot :hey buddy\\r"
+    echo -e ":a!b@c PRIVMSG #scheme-bots :rudybot: seen a?\\r"
 
     sleep 10
     
-    echo ":a!b@c PRIVMSG #scheme-bots :rudybot: news\\r"
-    echo ":a!b@c PRIVMSG #scheme-bots :rudybot: quote\\r"
-    echo ":a!b@c PRIVMSG rudybot :\001VERSION\001\\r"
-    echo ":a!b@c PRIVMSG rudybot :what ho, my good man\\r"
+    echo -e ":a!b@c PRIVMSG #scheme-bots :rudybot: news\\r"
+    echo -e ":a!b@c PRIVMSG #scheme-bots :rudybot: quote\\r"
+    echo -e ":a!b@c PRIVMSG rudybot :\001VERSION\001\\r"
+    echo -e ":a!b@c PRIVMSG rudybot :what ho, my good man\\r"
 
     while true
     do
