7bf05d4d5f88d54244c95a1b3e979e96546ba161
Eric Hanchrow <eric.hanchrow@gmail.com>
2007-09-17 17:15:56 +0000

    *sigh* new bug -- we get N responses to "quote", where N seems to be the number of channels we've joined.  At least I've updated dry-test.sh to repro it.

--- plt-scheme/irc/dry-test.sh
+++ plt-scheme/irc/dry-test.sh
@@ -3,26 +3,19 @@
 (
 
     echo -e ":localhost. 001 rudybot :Welcome to the Debian dancer-ircd Network rudybot \\r"
-    echo -e ":localhost 366 rudybot #scheme-bots :what's up, homes? \\r"
-    echo -e ":localhost 366 rudybot #emacs :what's up, homes? \\r"
-
-    echo -e ":a!b@c PRIVMSG #scheme-bots :hey buddy\\r"
-    echo -e ":a!b@c PRIVMSG #scheme-bots :rudybot: hey buddy\\r"
-    echo -e ":a!b@c PRIVMSG rudybot :hey buddy\\r"
-    echo -e ":a!b@c PRIVMSG #scheme-bots :rudybot: seen a?\\r"
+    
+    for c in squank wank hippy hoppy flippy floppy silly sally
+    do
+        echo -e ":localhost 366 rudybot #$c :what's up, homes? \\r"
+    done
 
     sleep 10
     
-    echo -e ":a!b@c PRIVMSG #scheme-bots :rudybot: news\\r"
     echo -e ":a!b@c PRIVMSG #scheme-bots :rudybot: quote\\r"
-    echo -e ":a!b@c PRIVMSG rudybot :\001VERSION\001\\r"
-    echo -e ":a!b@c PRIVMSG rudybot :what ho, my good man\\r"
 
-    sleep 10
-
-#     while true
-#     do
-#         sleep 120
-#     done
+    while true
+    do
+        sleep 120
+    done
 
     ) | ./run-bot.ss -c '#scheme-bots' -n rudybot
