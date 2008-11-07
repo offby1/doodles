fb1f52de3ebbbe8e71bf9281cde21b20092a30de
Eric Hanchrow <eric.hanchrow@gmail.com>
2008-11-07 06:43:07 +0000

    v4-ified

--- plt-scheme/all-unicode-chars.scm
+++ plt-scheme/all-unicode-chars.scm
@@ -1,15 +1,16 @@
 #! /bin/sh
 #| Hey Emacs, this is -*-scheme-*- code!
-exec mzscheme -qu "$0" ${1+"$@"}
+exec  mzscheme --require "$0" --main -- ${1+"$@"}
 |#
 
 ;;; unicode chars range from 0 through #x10FFFF inclusive, except for
 ;;; #xd800 through #xdfff inclusive.
 
-(module all-unicode-chars mzscheme
+#lang scheme
+(printf "-*-coding:utf-8-*-~%")
 (let loop ((chars-considered 0))
   (when (< chars-considered #x110000)
     (unless (<= #xd800 chars-considered #xdfff)
       (let ((c  (integer->char chars-considered)))
       (printf "~a ~a ~%"c  (char-general-category c))))
-    (loop (+ 1 chars-considered)))))
+    (loop (+ 1 chars-considered))))
