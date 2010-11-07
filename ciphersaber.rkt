99c37df3f6b1dd8eb14264ab4c7b8bbec36f6800
Eric Hanchrow <eric.hanchrow@gmail.com>
2010-11-06 23:05:14 -0700

    Begin fiddling with "CipherSaber"

---
+++ ciphersaber.rkt
@@ -0,0 +1,53 @@
+#! /bin/sh
+#| Hey Emacs, this is -*-scheme-*- code!
+#$Id$
+exec racket -l errortrace --require "$0" --main -- ${1+"$@"}
+|#
+
+;; http://ciphersaber.gurus.org/faq.html
+
+#lang racket
+(require rackunit rackunit/text-ui)
+
+;; Stolen from
+;; .../planet/300/5.0.1.900/cache/soegaard/math.plt/1/4/number-theory.ss
+(define (digits n base)
+  (define (d x)
+    (if (< x base)
+        (list x)
+        (cons (remainder x base)
+              (d (quotient x base)))))
+  (unless (integer? n)
+    (error 'digits "expected an integer, got: n"))
+  (reverse (d (if (negative? n) (- n) n))))
+
+(define (hex->integers s)
+   (digits (string->number s 16) 256))
+
+(define (leading-zero string)
+  (if (= 1 (string-length string))
+      (string-append "0" string)
+      string))
+
+(define-test-suite hmm-tests
+
+  (check-equal?
+
+   (string-upcase
+    (apply string-append
+           (reverse
+            (map (lambda (i)
+                   (leading-zero (number->string i 16)))
+                 (let ([ks-hex "eb9f7781b734ca72a719"]
+                       [plaintext-bytes #"Plaintext"])
+                   (for/fold ([output-bytes '()])
+                       ([pb (in-bytes plaintext-bytes)]
+                        [cb (in-list (hex->integers ks-hex))])
+                       (cons (bitwise-xor pb cb) output-bytes)))))))
+
+   "BBF316E8D940AF0AD3"))
+
+(define (main . args)
+  (exit (run-tests hmm-tests 'verbose)))
+
+(provide main)
