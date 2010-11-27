5813a96e6f927200371b14131e4a7346c63662d6
Eric Hanchrow <eric.hanchrow@gmail.com>
2010-11-26 17:48:47 -0800

    http://programmingpraxis.com/2010/11/26/divisors-and-totatives/

---
+++ divisors.rkt
@@ -0,0 +1,51 @@
+#! /bin/sh
+#| Hey Emacs, this is -*-scheme-*- code!
+exec racket -l errortrace --require "$0" --main -- ${1+"$@"}
+|#
+
+;; http://programmingpraxis.com/2010/11/26/divisors-and-totatives/
+
+#lang racket
+(require rackunit rackunit/text-ui
+         (only-in (planet soegaard/math/math)
+                  positive-divisors
+                  coprime?))
+
+(provide divisors)
+(define divisors positive-divisors)
+
+(define-test-suite divisors-tests
+  (check-equal? (apply set (divisors 20)) (set 1 2 4 5 10 20)))
+
+(provide sum-of-divisors)
+(define (sum-of-divisors n)
+  (apply + (divisors n)))
+
+(define (number-of-divisors n)
+  (length (divisors n)))
+
+(provide totatives)
+(define (totatives n)
+  (filter
+   (lambda (x) (coprime? n x))
+   (build-list (sub1 n) add1)))
+
+(define-test-suite totatives-tests
+  (check-equal? (apply set (totatives 30))
+                (set 1 7 11 13 17 19 23 29)))
+
+(provide totient)
+(define (totient n)
+  (length (totatives n)))
+
+(define-test-suite totient-tests
+  (check-equal? (totient 30) 8))
+
+(define-test-suite all-tests
+  divisors-tests
+  totatives-tests
+  totient-tests)
+
+(provide main)
+(define (main . args)
+  (exit (run-tests all-tests 'verbose)))
