fcc16b30ea09e97cbc5bd5faef248f3de940f962
Eric Hanchrow <eric.hanchrow@gmail.com>
2011-06-03 09:21:00 -0700

    Might's well use the official SICP language.

--- SICP/1.3.scm
+++ SICP/1.3.scm
@@ -1,34 +1,23 @@
-#! /bin/sh
-#| Hey Emacs, this is -*-scheme-*- code!
-exec racket --require "$0" --main -- ${1+"$@"}
-|#
-
-#lang racket
+#lang planet neil/sicp
 
 (define (square z)
   (* z z))
 
 (define (sum-of-squares-of-two-largest a b c)
 
-  (define smallest
-    (cond
-     ((<= a b c)
-      a)
-     ((<= b a c)
-      b)
-     (else
-      c)))
-
   (- (+ (square a)
         (square b)
         (square c))
-     (square smallest)))
+     (square (min a b c))))
 
-(provide main)
-(define (main . args)
-  (define (demo a b c)
-    (printf "~a ~a ~a => ~a~%" a b c (sum-of-squares-of-two-largest a b c)))
 
-  (demo 1 2 3)
-  (demo 3 2 1)
-  (demo 9 9 9))
+(define (demo a b c)
+  (for-each (lambda (thing )
+              (display thing)
+              (display " "))
+            (list a b c (sum-of-squares-of-two-largest a b c)))
+  (newline))
+
+(demo 1 2 3)
+(demo 3 2 1)
+(demo 9 9 9)
