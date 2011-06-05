1c5a913b048ecea3860ffa046062139422029245
Eric Hanchrow <eric.hanchrow@gmail.com>
2011-06-05 14:36:11 -0700

    channel->seq
    Something tells me I didn't have to write this myself, but I couldn't
    find anything like it in the manual, so ...

---
+++ plt-scheme/web/amazon/channel.rkt
@@ -0,0 +1,46 @@
+#! /bin/sh
+#| Hey Emacs, this is -*-scheme-*- code!
+exec racket -l errortrace --require "$0" --main -- ${1+"$@"}
+|#
+
+#lang racket
+(require
+ (only-in racket/async-channel
+          async-channel-get
+          async-channel-put
+          make-async-channel)
+ (only-in racket/generator
+          in-generator
+          yield)
+ rackunit
+ rackunit/text-ui
+ )
+
+(provide channel->seq)
+(define/contract (channel->seq ch)
+  (channel? . -> . sequence?)
+  (in-generator
+   (let loop ()
+     (let ([datum (async-channel-get ch)])
+       (when (not (eof-object? datum))
+         (yield datum)
+         (loop))))))
+
+(define-test-suite channel->seq-tests
+  (let ([ch (make-async-channel)])
+    (async-channel-put ch eof)
+    (check-equal? (sequence->list (channel->seq ch)) '()))
+
+  (let ([ch (make-async-channel)])
+    (for ([datum (list 1 2 3 'frotz eof)])
+      (async-channel-put ch datum))
+    (check-equal?
+     (sequence->list (channel->seq ch))
+     '(1 2 3 frotz)))  )
+
+(define-test-suite all-tests
+  channel->seq-tests)
+
+(provide main)
+(define (main . args)
+  (exit (run-tests all-tests 'verbose)))
