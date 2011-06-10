bfc35cc2c38dbcc1c40a7ee26e4f0b7122b9aa52
Eric Hanchrow <eric.hanchrow@gmail.com>
2011-06-09 20:13:30 -0700

    *** empty log message ***

--- plt-scheme/continue/blog.rkt
+++ plt-scheme/continue/blog.rkt
@@ -11,10 +11,30 @@
         (post "Second Post" "This is another post")))
 
 ; start: request -> response
-; Consumes a request, and produces a page that displays all of the
+; Consumes a request and produces a page that displays all of the
 ; web content.
 (define (start request)
-  (render-blog-page BLOG request))
+  (local [(define a-blog
+            (cond [(can-parse-post? (request-bindings request))
+                   (cons (parse-post (request-bindings request))
+                         BLOG)]
+                  [else
+                   BLOG]))]
+    (render-blog-page a-blog request)))
+
+
+; can-parse-post?: bindings -> boolean
+; Produces true if bindings contains values for 'title and 'body.
+(define (can-parse-post? bindings)
+  (and (exists-binding? 'title bindings)
+       (exists-binding? 'body bindings)))
+
+
+; parse-post: bindings -> post
+; Consumes a bindings, and produces a post out of the bindings.
+(define (parse-post bindings)
+  (post (extract-binding/single 'title bindings)
+        (extract-binding/single 'body bindings)))
 
 ; render-blog-page: blog request -> response
 ; Consumes a blog and a request, and produces an HTML page
@@ -22,8 +42,13 @@
 (define (render-blog-page a-blog request)
   (response/xexpr
    `(html (head (title "My Blog"))
-          (body (h1 "My Blog")
-                ,(render-posts a-blog)))))
+          (body
+           (h1 "My Blog")
+           ,(render-posts a-blog)
+           (form
+            (input ((name "title")))
+            (input ((name "body")))
+            (input ((type "submit"))))))))
 
 ; render-post: post -> xexpr
 ; Consumes a post, produces an xexpr fragment of the post.
@@ -31,6 +56,7 @@
   `(div ((class "post"))
         ,(post-title a-post)
         (p ,(post-body a-post))))
+
 
 ; render-posts: blog -> xexpr
 ; Consumes a blog, produces an xexpr fragment
