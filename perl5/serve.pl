c7644bbd3e4957793ac9a2821d4c1742d26a1922
Eric Hanchrow <eric.hanchrow@gmail.com>
2011-07-01 20:35:43 -0700

    This works too.

--- perl5/serve.pl
+++ perl5/serve.pl
@@ -1,49 +1,22 @@
 #!/usr/bin/perl
-{
-    package MyWebServer;
 
-    use HTTP::Server::Simple::CGI;
-    use base qw(HTTP::Server::Simple::CGI);
+use warnings;
+use strict;
 
-    my %dispatch = (
-        '/hello' => \&resp_hello,
-        # ...
-        );
+use Data::Dumper;
+use HTTP::Server::PSGI;
 
-    sub handle_request {
-        my $self = shift;
-        my $cgi  = shift;
+my $app = sub {
+    return [
+        '200',
+        [ 'Content-Type' => 'text/plain' ],
+        [  "app sez " . Data::Dumper->Dump ([@_], [qw(args)])],
+        ];
+};
 
-        my $path = $cgi->path_info();
-        my $handler = $dispatch{$path};
-
-        if (ref($handler) eq "CODE") {
-            print "HTTP/1.0 200 OK\r\n";
-            $handler->($cgi);
-
-        } else {
-            print "HTTP/1.0 404 Not found\r\n";
-            print $cgi->header,
-            $cgi->start_html(Not found),
-            $cgi->h1(Not found),
-            $cgi->end_html;
-        }
-    }
-
-    sub resp_hello {
-        my $cgi  = shift;       # CGI.pm object
-        return if !ref $cgi;
-
-        my $who = $cgi->param(name);
-
-        print $cgi->header,
-        $cgi->start_html("Hello"),
-        $cgi->h1("Hello $who!"),
-        $cgi->end_html;
-    }
-
-} 
-
-# start the server on port 8080
-my $pid = MyWebServer->new(8080)->background();
-print "Use kill $pid to stop server.\n";
+my $server = HTTP::Server::PSGI->new(
+    host => "127.0.0.1",
+    port => 8080,
+    timeout => 120,
+    );
+$server->run($app);
