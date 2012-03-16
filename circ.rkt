accbdbf53b5c7f2ffb86eea32ccb48a2d65bf658
Eric Hanchrow <eric.hanchrow@gmail.com>
2012-03-16 10:59:14 -0700

    hmm

--- circ.rkt
+++ circ.rkt
@@ -8,10 +8,36 @@ exec racket -l errortrace --require "$0" --main -- ${1+"$@"}
 
 (provide is-circular?)
 (define (is-circular? seq)
-  #f)
+  (cond
+   ((not (pair? seq))
+    #f)
+   ((not (pair? (cdr seq)))
+    #f)
+   (else
+    (let loop ([tortoise seq]
+               [hare (cdr seq)])
+      (cond
+       ((null? tortoise)
+        #f)
+       ((null? hare)
+        #f)
+       ((null? (cdr hare))
+        #f)
+       ((eq? tortoise hare)
+        #t)
+       (else
+        (loop (cdr tortoise)
+              (cddr hare)))
+       )))))
 
 (define-test-suite is-circular?-tests
-  (check-false (is-circular? '())))
+  (check-false (is-circular? '()))
+  (check-false (is-circular? '(1 2 3 2 1)))
+  (check-true (is-circular? (with-input-from-string "#0=(1 . #0#)"  read)))
+
+  ;; Not sure if this example is truly circular.
+  ;;(check-true (is-circular?  (with-input-from-string "#0=(1 2 3 #0# 4 5 6)"  read)))
+  )
 
 (define-test-suite all-tests
   is-circular?-tests)
