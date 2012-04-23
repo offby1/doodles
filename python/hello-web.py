ee5d2c727da500fc63b5205b92681238c9d6a098
Eric Hanchrow <eric.hanchrow@gmail.com>
2012-04-23 14:07:46 -0700

    Track all calls by URL

--- python/hello-web.py
+++ python/hello-web.py
@@ -9,17 +9,20 @@ from __future__ import absolute_import
 
 __author__ = 'erich@cozi.com'
 
+from collections import defaultdict
+from webob import Response
 from webob.dec import wsgify
 import wsgiref.simple_server
 
 class Simple(object):
     def __init__(self):
-        self.counter = 0
+        self.counts_by_URL = defaultdict(int)
     
     @wsgify
     def __call__(self, req):
-        self.counter += 1
-        return "I have been called {self.counter} times".format(**locals())
+        self.counts_by_URL[req.path] += 1
+        return Response("My call history: " + str(self.counts_by_URL),
+                        content_type='text/plain')
 
 if __name__ == "__main__":
     httpd = wsgiref.simple_server.make_server('localhost', 8000, Simple())
