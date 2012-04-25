114434e2f55009a15612083618eac4c120e0055b
Eric Hanchrow <eric.hanchrow@gmail.com>
2012-04-25 14:49:51 -0700

    https://developers.google.com/appengine/docs/python/gettingstarted/usingwebapp

--- google-app-engine/helloworld.py
+++ google-app-engine/helloworld.py
@@ -1,5 +1,17 @@
-# https://developers.google.com/appengine/docs/python/gettingstarted/helloworld
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
 
-print 'Content-Type: text/plain'
-print ''
-print 'Hello, world!'
+class MainPage(webapp.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.out.write('Hello, webapp World!')
+
+application = webapp.WSGIApplication(
+                                     [('/', MainPage)],
+                                     debug=True)
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
+    main()
