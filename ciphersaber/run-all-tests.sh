196f5295f67c9116dbac4377bdf90c40a5963a3f
Eric Hanchrow <eric.hanchrow@gmail.com>
2012-05-05 09:50:14 -0700

    Use a portable "perm" expression
    Now it works on OS X

--- ciphersaber/run-all-tests.sh
+++ ciphersaber/run-all-tests.sh
@@ -4,7 +4,9 @@ here=$(cd $(dirname $0); pwd)
 
 set -e
 
-for file in $(find $here -maxdepth 1 -name '*.rkt' -perm /u=x)
+for file in $(find $here -maxdepth 1 -name '*.rkt' -perm +100)
 do
+    echo "$file ..."
     $file -t
+    echo
 done
