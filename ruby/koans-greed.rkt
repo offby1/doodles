324726a7888a62856b40ccbc916bb5ea9fdec214
Eric Hanchrow <eric.hanchrow@gmail.com>
2012-05-06 14:48:09 -0700

    Ahh, now it's functional

--- ruby/koans-greed.rkt
+++ ruby/koans-greed.rkt
@@ -8,26 +8,19 @@
       (hash-update rv elt add1 0)))
 
 (define (score counts-by-number)
-  (let (
-        ;; make counts-by-number mutable because I'm a lazy slob
-        [counts-by-number (make-hash (dict-map counts-by-number cons))]
-
-        [total 0])
-
-    ;; three ones: 1,000
-
-    ;; three of anything else: 100 * n
-    (for ([(num count) (in-hash counts-by-number)])
-      (when (<= 3 count)
-        (hash-update! counts-by-number num (curryr - 3))
-        (set! total (+ total (* num (if (= num 1) 1000 100))))))
-
-    ;; leftover ones: 100 apiece
-    (set! total (+ total (* 100 (hash-ref counts-by-number 1 0))))
-    ;; leftover fives: 50 apiece
-    (set! total (+ total (* 50  (hash-ref counts-by-number 5 0))))
-
-    total))
+  (for/sum
+   ([(num count) (in-hash counts-by-number)])
+   (let ([score-from-triples
+          (if (<= 3 count)
+              (* num (if (= num 1) 1000 100))
+              0)]
+         [score-from-leftovers
+          (* (if (<= 3 count) (- count 3) count)
+             (case num
+               ((1) 100)
+               ((5)  50)
+               (else 0)))])
+     (+ score-from-triples score-from-leftovers))))
 
 (define-simple-check (check-score throws expected-score)
   (check-equal? (score (multiset throws)) expected-score))
