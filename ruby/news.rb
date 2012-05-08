ea7fb79193437303e1f09814af448eb00be38222
Eric Hanchrow <eric.hanchrow@gmail.com>
2012-05-07 19:33:05 -0700

    Handle redirects.

--- ruby/news.rb
+++ ruby/news.rb
@@ -1,16 +1,29 @@
 require 'net/http'
 require 'rexml/document'
 
-def gimme
-  extract_and_process(call_http_xml('http://feeds.nytimes.com/nyt/rss/HomePage'))
+class NewsError < Exception
 end
 
-def call_http_xml(uri)
+def gimme
+  extract_and_process(call_http_xml('http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml'))
+end
+
+def call_http_xml(uri, redirection_limit = 10)
+  raise NewsError, "too many redirects" if redirection_limit == 0
+
   url = URI.parse(uri)
   client = Net::HTTP.new(url.host, url.port)
   res = client.request(Net::HTTP::Get.new(uri))
-  doc = REXML::Document.new(res.body)
-  doc
+  case res
+  when Net::HTTPSuccess then
+    return REXML::Document.new(res.body)
+  when Net::HTTPRedirection then
+    location = res['location']
+    warn "Redirected to #{location}"
+    call_http_xml(location, redirection_limit - 1)
+  else
+    raise NewsError, "Bad response code", res.value
+  end
 end
 
 def extract_and_process(doc)
