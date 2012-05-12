1b5a33469fa466bf3775cb97d279182f6e1df50c
Eric Hanchrow <eric.hanchrow@gmail.com>
2012-05-12 16:03:01 -0700

    Hmm, a failing test

--- cp.rkt
+++ cp.rkt
@@ -1,51 +1,8 @@
 #lang racket
 
-(require rackunit rackunit/text-ui unstable/debug)
+(require rackunit rackunit/text-ui)
 
 ;; http://en.wikipedia.org/wiki/Cartesian_product
-
-(define (cp-helper s1 s2)
-  (apply
-   set
-   (for*/list ([one s1]
-               [two s2])
-              (set one two))))
-
-(define-test-suite cartesian-product-tests
-  (check-equal? (cp-helper (set) (set 1 2))
-                (set))
-
-  (check-equal? (cp-helper (set 1) (set 2))
-                (set (set 1 2)))
-
-  (check-equal? (cp-helper (set 1 2) (set 'dog 'cat))
-                (set (set 1 'dog)
-                     (set 1 'cat)
-                     (set 2 'dog)
-                     (set 2 'cat))))
-
-(define (embiggen prod new)
-  (apply
-   set-union
-   (for/list ([old  prod])
-     (cp-helper old new))))
-
-(define-test-suite embiggen-tests
-  (check-equal?
-   (embiggen  (set (set 1 2)) (set 'frotz))
-
-   (set
-    (set 1 'frotz)
-    (set 2 'frotz)))
-
-  (check-equal?
-   (embiggen (cp-helper (set 1 2) (set 'dog 'cat)) (set 'salad))
-
-   (set
-    (set 1 'salad)
-    (set 2 'salad)
-    (set 'dog 'salad)
-    (set 'cat 'salad))))
 
 (define (cartesian-product . sets)
   (cond
@@ -56,7 +13,14 @@
    (else
     (for/fold ([rv (set (car sets))])
         ([s (cdr sets)])
-        (embiggen rv s)))))
+        (apply
+         set-union
+         (for/list ([old  rv])
+           (apply
+            set
+            (for*/list ([one old]
+                        [two s])
+                       (set one two)))))))))
 
 (define-test-suite nary-tests
   (check-equal? (cartesian-product)
@@ -65,21 +29,22 @@
   (check-equal? (cartesian-product (set 1 2))
                 (set (set 1 2)))
 
-  (check-equal? (cartesian-product (set 1 2) (set 'frotz))
-                (cp-helper (set 1 2)
-                           (set 'frotz)))
-
   (check-equal? (cartesian-product (set 1 2) (set 'red 'blue) (set 'fish))
 
                 (set
                  (set 1 'fish)
                  (set 2 'fish)
                  (set 'red 'fish)
-                 (set 'blue 'fish))))
+                 (set 'blue 'fish)))
 
-(run-tests
- (test-suite
-  "all"
-  cartesian-product-tests
-  embiggen-tests
-  nary-tests))
+  (check-equal? (cartesian-product
+                 (set 1 2 3)
+                 (set 'two)
+                 (set 'three))
+                (set
+                 (set 1 'two 'three)
+                 (set 2 'two 'three)
+                 (set 3 'two 'three))
+                )
+  )
+(run-tests nary-tests)
