592b7d7ba45c6de2bb4f87b80c13a0f6c6080120
Eric Hanchrow <eric.hanchrow@gmail.com>
2012-05-12 15:56:00 -0700

    Rename

--- cp.rkt
+++ cp.rkt
@@ -2,12 +2,9 @@
 
 (require rackunit rackunit/text-ui unstable/debug)
 
-(define cp? (set/c set?))
-
 ;; http://en.wikipedia.org/wiki/Cartesian_product
 
-(define/contract (cartesian-product s1 s2)
-  (set? set? . -> . cp?)
+(define (cp-helper s1 s2)
   (apply
    set
    (for*/list ([one s1]
@@ -15,25 +12,23 @@
               (set one two))))
 
 (define-test-suite cartesian-product-tests
-  (check-equal? (cartesian-product (set) (set 1 2))
-                 (set))
+  (check-equal? (cp-helper (set) (set 1 2))
+                (set))
 
-  (check-equal? (cartesian-product (set 1) (set 2))
+  (check-equal? (cp-helper (set 1) (set 2))
                 (set (set 1 2)))
 
-  (check-equal? (cartesian-product (set 1 2) (set 'dog 'cat))
+  (check-equal? (cp-helper (set 1 2) (set 'dog 'cat))
                 (set (set 1 'dog)
-                         (set 1 'cat)
-                         (set 2 'dog)
-                         (set 2 'cat))))
+                     (set 1 'cat)
+                     (set 2 'dog)
+                     (set 2 'cat))))
 
-(define/contract (embiggen prod new)
-  (cp? set? . -> . cp?)
-
+(define (embiggen prod new)
   (apply
    set-union
    (for/list ([old  prod])
-      (cartesian-product old new))))
+     (cp-helper old new))))
 
 (define-test-suite embiggen-tests
   (check-equal?
@@ -44,44 +39,43 @@
     (set 2 'frotz)))
 
   (check-equal?
-   (embiggen (cartesian-product (set 1 2) (set 'dog 'cat)) (set 'salad))
+   (embiggen (cp-helper (set 1 2) (set 'dog 'cat)) (set 'salad))
 
-    (set
-     (set 1 'salad)
-     (set 2 'salad)
-     (set 'dog 'salad)
-     (set 'cat 'salad))))
+   (set
+    (set 1 'salad)
+    (set 2 'salad)
+    (set 'dog 'salad)
+    (set 'cat 'salad))))
 
-(define/contract (nary-cartesian-product . sets)
-  (->* () #:rest (listof set?) cp?)
+(define (cartesian-product . sets)
   (cond
    ((null? sets)
-     (set))
+    (set))
    ((null? (cdr sets))
     (set (car sets)))
    (else
-    (for/fold ([rv  (set (car sets))])
+    (for/fold ([rv (set (car sets))])
         ([s (cdr sets)])
         (embiggen rv s)))))
 
 (define-test-suite nary-tests
-  (check-equal? (nary-cartesian-product)
-                 (set))
+  (check-equal? (cartesian-product)
+                (set))
 
-  (check-equal? (nary-cartesian-product (set 1 2))
-                 (set (set 1 2)))
+  (check-equal? (cartesian-product (set 1 2))
+                (set (set 1 2)))
 
-  (check-equal? (nary-cartesian-product (set 1 2) (set 'frotz))
-                (cartesian-product (set 1 2)
-                                   (set 'frotz)))
+  (check-equal? (cartesian-product (set 1 2) (set 'frotz))
+                (cp-helper (set 1 2)
+                           (set 'frotz)))
 
-  (check-equal? (nary-cartesian-product (set 1 2) (set 'red 'blue) (set 'fish))
+  (check-equal? (cartesian-product (set 1 2) (set 'red 'blue) (set 'fish))
 
-                 (set
-                  (set 1 'fish)
-                  (set 2 'fish)
-                  (set 'red 'fish)
-                  (set 'blue 'fish))))
+                (set
+                 (set 1 'fish)
+                 (set 2 'fish)
+                 (set 'red 'fish)
+                 (set 'blue 'fish))))
 
 (run-tests
  (test-suite
