ffa3c5a9776da6a87d51fcfcedebdfb774cd0838
Eric Hanchrow <eric.hanchrow@gmail.com>
2012-06-10 08:09:57 -0700

    Simple and correct

--- cp.rkt
+++ cp.rkt
@@ -1,50 +1,56 @@
 #lang racket
 
-(require rackunit rackunit/text-ui)
+(module+ test
+(require rackunit))
 
 ;; http://en.wikipedia.org/wiki/Cartesian_product
 
+(define (extend accumlated elt)
+  (for*/fold ([result (set)])
+      ([old-set accumlated]
+       [new-item elt])
+      (set-add result (set-add old-set new-item))))
+(module+ test
+(check-equal? (extend (set (set)) (set 1 2 3))
+              (set (set 1) (set 2) (set 3)))
+(check-equal? (extend (set (set 1 2) (set 3 4) (set 5 6)) (set 'cat 'dog))
+              (set
+               (set 1 2 'cat)
+               (set 1 2 'dog)
+               (set 3 4 'cat)
+               (set 3 4 'dog)
+               (set 5 6 'cat)
+               (set 5 6 'dog)
+               )))
+
 (define (cartesian-product . sets)
-  (cond
-   ((null? sets)
-    (set))
-   ((null? (cdr sets))
-    (set (car sets)))
-   (else
-    (for/fold ([rv (set (car sets))])
-        ([s (cdr sets)])
-        (apply
-         set-union
-         (for/list ([old  rv])
-           (apply
-            set
-            (for*/list ([one old]
-                        [two s])
-                       (set one two)))))))))
+  (for/fold ([result (set (set))])
+      ([elt sets])
+      (extend result elt)))
 
-(define-test-suite nary-tests
-  (check-equal? (cartesian-product)
-                (set))
+(module+ test
+(check-equal? (cartesian-product)
+              (set (set)))
 
-  (check-equal? (cartesian-product (set 1 2))
-                (set (set 1 2)))
+(check-equal? (cartesian-product (set 1 2))
+              (set (set 1) (set 2)))
 
-  (check-equal? (cartesian-product (set 1 2) (set 'red 'blue) (set 'fish))
+(check-equal? (cartesian-product (set 1 2) (set 'red 'blue) (set 'fish))
 
-                (set
-                 (set 1 'fish)
-                 (set 2 'fish)
-                 (set 'red 'fish)
-                 (set 'blue 'fish)))
+              (set
+               (set 1 'red 'fish)
+               (set 2 'red 'fish)
+               (set 1 'blue 'fish)
+               (set 2 'blue 'fish)))
 
-  (check-equal? (cartesian-product
-                 (set 1 2 3)
-                 (set 'two)
-                 (set 'three))
-                (set
-                 (set 1 'two 'three)
-                 (set 2 'two 'three)
-                 (set 3 'two 'three))
-                )
-  )
-(run-tests nary-tests)
+(check-equal? (cartesian-product
+               (set 1 2 3)
+               (set 'two)
+               (set 'three)
+               (set 'four)
+               (set 'five)
+               (set 'six))
+              (set
+               (set 1 'two 'three 'four 'five 'six)
+               (set 2 'two 'three 'four 'five 'six)
+               (set 3 'two 'three 'four 'five 'six))))
