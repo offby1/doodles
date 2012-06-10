8630be5394116531cf0894ce2a03d1b81748dd64
Eric Hanchrow <eric.hanchrow@gmail.com>
2012-06-09 19:10:08 -0700

    Put some tests back, using the new submodule feature.
    Run 'em with "raco test ./partition.rkt"

--- partition.rkt
+++ partition.rkt
@@ -2,12 +2,19 @@
 
 ;; http://en.wikipedia.org/wiki/Partition_(number_theory)
 
+(module+ test (require rackunit))
+
 ;; Return a vector that is like V, except the INDEXth number is one
 ;; bigger.
 (define (increment-at v index)
   (let ([v (vector-copy v)])
     (dict-update! v index add1)
     v))
+
+(module+ test
+(check-equal?
+ (increment-at (vector 1 2 3) 1)
+ (vector 1 3 3)))
 
 ;; Given a partition P which sums to n, return a set of all partitions
 ;; which sum to n + 1.
@@ -29,3 +36,11 @@
           ([result (set)])
           ([p (all-partitions (sub1 n))])
        (set-union result (more-partitions p)))))
+
+(module+ test
+(for ([(expected index) (in-indexed '[1 1 2 3 5 7 11 15 22 30 42 56])])
+  (when (positive? index)
+    (check-equal? (set-count (all-partitions index)) expected))))
+
+(module+ main
+(pretty-print (all-partitions 10)))
