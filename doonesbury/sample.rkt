e067ccb91506e881dab48d439ab905df4b3d9422
Eric Hanchrow <eric.hanchrow@gmail.com>
2013-03-08 21:33:06 -0800

    Thinking about using "match" to destructure the XML

--- doonesbury/sample.rkt
+++ doonesbury/sample.rkt
@@ -3,26 +3,33 @@
 (require (planet clements/sxml2:1:=3)
          racket/pretty)
 
-(provide text-transformer)
-(define (text-transformer tag-symbol text-transform-function)
-    `((,tag-symbol . ,(lambda (tag . elements)
-                        (cons tag (map
-                                   (lambda (elt)
-                                     (match elt
-                                       [(list '*text* (? string? str))
-                                        (list '*text* (text-transform-function str))]
-                                       [#t elt])
-                                     )
-                                   elements))))
-      (*default* . ,(lambda args args))))
+(define (string-reverse s)
+  (list->string (reverse (string->list s))))
 
 (module+ main
   (define sample-doc
     (call-with-input-file "sample-rss-feed.xml"
-      (curryr ssax:xml->sxml '((x . "http://purl.org/rss/1.0/")))))
+      (curryr ssax:xml->sxml '(
+                               (x . "http://purl.org/rss/1.0/")
+                               (rdf . "http://www.w3.org/1999/02/22-rdf-syntax-ns#")
+                               ))))
 
-  (define (string-reverse str)
-    (list->string (reverse (string->list str))))
+  (pretty-print sample-doc)
 
   (pretty-print
-   (pre-post-order sample-doc (text-transformer 'x:link string-reverse))))
+   (for/list ([thing sample-doc])
+     (match thing
+       [(list 'rdf:RDF rdf-things ...)
+        (cons 'rdf:RDF
+              (for/list ([thing rdf-things])
+                (match thing
+                  [(list 'x:item items ...)
+                   (cons 'x:item
+                         (for/list ([item items])
+                           (match item
+                             [(list 'x:link link) (list 'x:link (string-reverse link))]
+                             [_ item])))
+                   ]
+                  [_ thing])))]
+
+       [_ thing]))))
