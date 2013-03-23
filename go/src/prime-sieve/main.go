56471b2dd09351441d72264cbfc61efab98248ce
Eric Hanchrow <eric.hanchrow@gmail.com>
2013-03-23 13:07:13 -0700

    Yay, I cn hz goroutine

--- go/src/prime-sieve/main.go
+++ go/src/prime-sieve/main.go
@@ -6,4 +6,19 @@ import (
 
 func main() {
 	fmt.Printf("Ahoy y'all\n")
+	int_channel := integers()
+	fmt.Printf("Here's an int: %v\n", <-int_channel)
+	fmt.Printf("Here's another: %v\n", <-int_channel)
+}
+
+func integers() <-chan int {
+	ch := make(chan int)
+	go func() {
+		i := 2
+		for {
+			ch <- i
+			i += 1
+		}
+	}()
+	return ch
 }
