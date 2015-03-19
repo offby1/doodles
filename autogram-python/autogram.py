44416d8fbd703d3021f7c0187b2cf4f941290f40
Eric Hanchrow <eric.hanchrow@gmail.com>
2015-03-19 06:31:31 -0700

    Rename some functions and variables; other cosmetic tweaks

--- autogram-python/autogram.py
+++ autogram-python/autogram.py
@@ -21,17 +21,18 @@ import num2words                # pip install num2words
 
 lc_letters = sorted(set(string.letters.lower()))
 
-def survey_sentence(s):
+
+def string_to_counter(s):
     return collections.Counter(filter(str.isalpha, str(s).lower()))
 
 
-def perturb_survey(s):
-    for key in s:
-        s[key] = max(s[key] + random.randrange(-10, 10), 0)
-    return s
+def perturb_counter(c):
+    for key in c:
+        c[key] = max(c[key] + random.randrange(-10, 10), 0)
+    return c
 
 
-def survey_to_string(s, template):
+def counter_to_string(c, template):
     def pluralize(string, number):
         if number == 1:
             return string
@@ -40,36 +41,37 @@ def survey_to_string(s, template):
     def slot(letter, number):
         return '{} {}'.format(num2words.num2words(number), pluralize(letter, number))
 
-    return template.format(**{letter: slot(letter, s.get(letter, 0)) for letter in lc_letters})
+    return template.format(**{letter: slot(letter, c.get(letter, 0)) for letter in lc_letters})
 
 
 def template_snippet(l):
-    return "{%s}" % (l, )
+    return "{{{}}}".format(l)
 
 if __name__ == "__main__":
-    all_snippets = [template_snippet(l) for l in lc_letters[:10]]
+    all_snippets = [template_snippet(l) for l in lc_letters[:18]]
 
-    default_values = {letter: 0 for letter in lc_letters}
+    initial_counter = {letter: 0 for letter in lc_letters}
 
     snippets_plus_and = all_snippets
     snippets_plus_and[-1] = 'and ' + snippets_plus_and[-1]
-    template = "This sentence has {}.".format(', '.join(snippets_plus_and))
+    template = "This sentence, over which I slaved and struggled for many years, has {}.".format(', '.join(snippets_plus_and))
 
-    old = survey_to_string(survey_sentence(template.format(**default_values)), template)
+    old = counter_to_string(string_to_counter(template.format(**initial_counter)), template)
 
     seen = set([old])
 
     while True:
-        survey = survey_sentence(old)
-        new = survey_to_string(survey, template)
+        survey = string_to_counter(old)
+        new = counter_to_string(survey, template)
 
         if old == new:
+            print(len(seen))
             print(new)
             break
 
         while new in seen:
-            survey = perturb_survey(survey)
-            new = survey_to_string(survey, template)
+            survey = perturb_counter(survey)
+            new = counter_to_string(survey, template)
 
         seen.add(new)
         if len(seen) % 1000 == 0:
