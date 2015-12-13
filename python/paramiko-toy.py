d16edbfbab8897060cecf8620ebdfec035c97cd8
Eric Hanchrow <eric.hanchrow@gmail.com>
2015-12-12 19:16:21 -0800

    Look in the ssh-agent too

--- python/paramiko-toy.py
+++ python/paramiko-toy.py
@@ -1,5 +1,7 @@
+import codecs
 import os.path
-import paramiko
+import paramiko                 # pip install paramiko
+import sys
 
 
 def from_named_file(fn):
@@ -18,8 +20,16 @@ def from_named_dir(d):
         except (paramiko.ssh_exception.PasswordRequiredException,
                 paramiko.ssh_exception.SSHException,
                 UnicodeDecodeError) as e:
-            print('{}: {}'.format(p, e))
+            print('{}: {}'.format(p, e), file=sys.stderr)
+
+
+def key2str(pk):
+    return '{} bits'.format(pk.get_bits())
 
 if __name__ == "__main__":
     for name, result in from_named_dir(os.path.expanduser("~/.ssh")):
-        print('{}: {} bits'.format(name, result.get_bits()))
+        print('{}: {}'.format(name, key2str(result)))
+
+    agent = paramiko.agent.Agent()
+    for pk in agent.get_keys():
+        print('{}: {}'.format(codecs.encode(pk.get_fingerprint(), 'hex'), key2str(pk)))
