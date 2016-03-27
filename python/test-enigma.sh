9ad6b45d894c0734c2ab047bed54b73da3ae73f1
Eric Hanchrow <eric.hanchrow@gmail.com>
2016-03-27 11:35:28 -0700

    Print some status messages

--- python/test-enigma.sh
+++ python/test-enigma.sh
@@ -11,9 +11,17 @@ go()
     python3 "$here/enigma.py"
 }
 
+log ()
+{
+    echo "$@" > /dev/stderr
+}
+
 go < "$inf" | go > "$outf"
+log "This should be the first two lines of ${inf}:"
 head -2 "$outf"
+log "======================="
 cmp --quiet "$inf" "$outf" || exit 1
 
+log "Encrypting /usr/share/dict/words, just to see how long it takes ..."
 time go < /usr/share/dict/words | go > /dev/null
 
