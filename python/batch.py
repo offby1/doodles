7a42f59d764cdec1971326216c7aa3db846a16a5
Eric Hanchrow <eric.hanchrow@gmail.com>
2016-04-29 21:09:29 -0700

    Batch 'em up, boys

---
+++ python/batch.py
@@ -0,0 +1,10 @@
+# from http://stackoverflow.com/a/8290514/20146
+from itertools import islice, chain
+
+def batch(iterable, size):
+    sourceiter = iter(iterable)
+    while True:
+        batchiter = islice(sourceiter, size)
+        yield tuple(chain([batchiter.next()], batchiter))
+
+print(list(batch(range(10), 3)))
