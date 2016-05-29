d98df02d93dafe1a0f0528fc12cee49b3c31c456
Eric Hanchrow <eric.hanchrow@gmail.com>
2016-05-29 08:15:27 -0700

    Works, but takes two minutes to get to 2001

--- python/prime-sieve.py
+++ python/prime-sieve.py
@@ -1,26 +1,39 @@
-def integers():
+import queue
+from threading import Thread
+
+def fire_and_forget(callable):
+    T = Thread(target=callable)
+    T.start()
+
+
+def integers(sink):
     i = 2
     while True:
-        yield i
+        sink.put(i)
         i += 1
 
 
-def filter_sequence(input, predicate):
-    for item in input:
+def filter_sequence(source, sink, predicate):
+    while True:
+        item = source.get()
         if predicate(item):
-            yield item
+            sink.put(item)
 
 
 def is_multiple_of(x, factor):
     return (x % factor) == 0
 
 
-seq = filter_sequence(integers(), lambda x: not (is_multiple_of(x, 2)))
-while True:
-    o = next(seq)
-    print(o)
+q = queue.Queue()
+fire_and_forget(lambda : integers(q))
 
-    if o > 2000:
+while True:
+    prime = q.get()
+    print(prime)
+
+    if prime > 2000:
         break
 
-    seq = filter_sequence(iter(seq), lambda x: not (is_multiple_of(x, o)))
+    q2 = queue.Queue()
+    fire_and_forget(lambda : filter_sequence(q, q2, lambda x: not (is_multiple_of(x, prime))))
+    q = q2
