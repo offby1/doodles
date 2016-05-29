53455e7b0ff82a696c71880666e185a75a8707bd
Eric Hanchrow <eric.hanchrow@gmail.com>
2016-05-29 09:58:28 -0700

    Fast. Simple.  Gets to 3500; then blows up.
    From https://gist.githubusercontent.com/encukou/9747421/raw/4f70af2f64b7d2c09e6f0a0820997e82bf010786/sieve.py

--- python/prime-sieve.py
+++ python/prime-sieve.py
@@ -1,40 +1,7 @@
-import queue
-from concurrent.futures import ThreadPoolExecutor
+def sieve(ints):
+    prime = next(ints)
+    yield prime
+    yield from sieve(i for i in ints if i % prime)
 
-executor = ThreadPoolExecutor()
-
-def fire_and_forget(callable):
-    executor.submit(callable)
-
-
-def integers(sink):
-    i = 2
-    while True:
-        sink.put(i)
-        i += 1
-
-
-def filter_sequence(source, sink, predicate):
-    while True:
-        item = source.get()
-        if predicate(item):
-            sink.put(item)
-
-
-def is_multiple_of(x, factor):
-    return (x % factor) == 0
-
-
-q = queue.Queue()
-fire_and_forget(lambda : integers(q))
-
-while True:
-    prime = q.get()
-    print(prime)
-
-    if prime > 2000:
-        break
-
-    q2 = queue.Queue()
-    fire_and_forget(lambda : filter_sequence(q, q2, lambda x: not (is_multiple_of(x, prime))))
-    q = q2
+for i in sieve(iter(range(2, 30000))):
+    print(i)
