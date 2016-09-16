577d39674d72390fd29bdad53982c8caca8e9488
Eric Hanchrow <eric.hanchrow@gmail.com>
2016-09-15 19:48:11 -0700

    Add asyncio and futures

--- python/concurrency-example.py
+++ python/concurrency-example.py
@@ -3,10 +3,16 @@ obviously a win.  We will download a pile of web pages, and find that
 doing so concurrently is faster than doing them in sequence.  """
 
 # Core
+import asyncio
+import concurrent.futures
+import contextlib
 import queue
+import signal
+import sys
 import threading
 
 # 3rd-party
+import aiohttp                  # pip install aiohttp
 import requests                 # pip install requests
 
 urls = ('https://en.wikipedia.org/wiki/American_Eagles',
@@ -21,17 +27,18 @@ urls = ('https://en.wikipedia.org/wiki/American_Eagles',
         'https://en.wikipedia.org/wiki/Eagles_(1984_film)',
         'https://en.wikipedia.org/wiki/Eagles_(2012_film)',
 
-        # 'https://en.wikipedia.org/wiki/Eagles_(album)',
-        # 'https://en.wikipedia.org/wiki/Eagles_(band)',
-        # 'https://en.wikipedia.org/wiki/Eagles_(box_set)',
-        # 'https://en.wikipedia.org/wiki/Eagles_cricket_team',
-        # 'https://en.wikipedia.org/wiki/Eastern_Michigan_Eagles',
-        # 'https://en.wikipedia.org/wiki/Eastern_Washington_Eagles',
-        # 'https://en.wikipedia.org/wiki/Embry%E2%80%93Riddle_Aeronautical_University',
-        # 'https://en.wikipedia.org/wiki/Fraternal_Order_of_Eagles',
-        # 'https://en.wikipedia.org/wiki/Georgia_Southern_Eagles',
-        # 'https://en.wikipedia.org/wiki/Germany_national_football_team',
-        # 'https://en.wikipedia.org/wiki/Greg_Eagles',
+        'https://en.wikipedia.org/wiki/Eagles_(album)',
+        'https://en.wikipedia.org/wiki/Eagles_(band)',
+        'https://en.wikipedia.org/wiki/Eagles_(box_set)',
+        'https://en.wikipedia.org/wiki/Eagles_cricket_team',
+        'https://en.wikipedia.org/wiki/Eastern_Michigan_Eagles',
+        'https://en.wikipedia.org/wiki/Eastern_Washington_Eagles',
+        'https://en.wikipedia.org/wiki/Embry%E2%80%93Riddle_Aeronautical_University',
+        'https://en.wikipedia.org/wiki/Fraternal_Order_of_Eagles',
+        'https://en.wikipedia.org/wiki/Georgia_Southern_Eagles',
+        'https://en.wikipedia.org/wiki/Germany_national_football_team',
+        'https://en.wikipedia.org/wiki/Greg_Eagles',
+
         # 'https://en.wikipedia.org/wiki/Hanwha_Eagles',
         # 'https://en.wikipedia.org/wiki/Jeanne_Eagels',
         # 'https://en.wikipedia.org/wiki/Manly-Warringah_Sea_Eagles',
@@ -60,12 +67,13 @@ urls = ('https://en.wikipedia.org/wiki/American_Eagles',
         # 'https://en.wikipedia.org/wiki/Woodville-West_Torrens_Eagles',
 )
 
+def download_one(url):
+    return '{} => {} bytes'.format(url, len(requests.get(url).text))
+
 
 def naive_download(urls):
     for url in urls:
-        print(url, end='... ')
-        requests.get(url)
-        print()
+        print(download_one(url))
 
 
 def threaded_download(urls):
@@ -78,8 +86,7 @@ def threaded_download(urls):
             super(Download, self).__init__()
 
         def run(self):
-            resp = requests.get(self.url)
-            result_queue.put((self.url, len(resp.text.split())))
+            result_queue.put(download_one(self.url))
 
     for url in urls:
         t = Download(url)
@@ -88,13 +95,46 @@ def threaded_download(urls):
     for t in threads:
         t.join()
 
-    return result_queue
+    while not result_queue.empty():
+        print(result_queue.get())
+
+
+def future_download(urls):
+    with concurrent.futures.ThreadPoolExecutor(max_workers=len(urls)) as executor:
+        futures = [executor.submit(download_one, url) for url in urls]
+
+        for future in concurrent.futures.as_completed(futures):
+            print(future.result())
+
+
+async def async_download(client, url):
+    async with client.get(url) as response:
+        assert response.status == 200
+        text = await response.text()
+    print('{} => {} bytes'.format(url, len(text)))
+
 
 if __name__ == "__main__":
-    # real	0m7.146s
-    # naive_download(urls)
+    import timeit
+    def t(description, python_expression):
+        print(description)
+        print(timeit.timeit(python_expression, globals=globals(), number=1))
 
-    # real	0m1.199s
-    q = threaded_download(urls)
-    while not q.empty():
-        print(q.get())
+
+    t("naive:"   , 'naive_download(urls)')
+    t("threaded:", 'threaded_download(urls)')
+    t("futures:" ,'future_download(urls)')
+
+    with contextlib.closing(asyncio.get_event_loop()) as loop:
+        with contextlib.closing(aiohttp.ClientSession(loop=loop)) as client:
+
+            def signal_handler(signal, frame):
+                loop.stop()
+                client.close()
+                sys.exit(0)
+
+            signal.signal(signal.SIGINT, signal_handler)
+
+            tasks = [asyncio.ensure_future(async_download(client, u)) for u in urls]
+
+            t("asyncio:", 'loop.run_until_complete(asyncio.gather(*tasks))')
