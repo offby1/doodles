5d1c7571c922de956dc14cae9593ba6f28f00c4e
Eric Hanchrow <eric.hanchrow@gmail.com>
2016-10-10 21:49:48 -0700

    Use json & simplify

--- python/flickr.py
+++ python/flickr.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python
 
-"""Some documentation would be nice!"""
-
 from __future__ import print_function
 from __future__ import absolute_import
 
-__author__ = 'eric.hanchrow@gmail.com'
-
 # Core
-import collections
 import datetime
 import os
 import pprint
@@ -17,6 +12,8 @@ import sys
 # 3rd-party
 import configobj                # pip install configobj
 import flickrapi                # pip install flickrapi
+
+__author__ = 'eric.hanchrow@gmail.com'
 
 
 def get_auth_stuff(filename=None):
@@ -30,47 +27,30 @@ def get_auth_stuff(filename=None):
 
 api_key, shared_secret = get_auth_stuff()
 
-flickr = flickrapi.FlickrAPI(api_key, shared_secret)
+flickr = flickrapi.FlickrAPI(api_key,
+                             shared_secret,
+                             format='parsed-json',
+                             cache=True)
 
-
-def dump(thing, indent=0):
-    print(' ' * indent, end='')
-    print(thing.__class__.__name__,
-          repr(thing.tag),
-          repr(thing.text),
-          thing.items())
-    for sub in thing:
-        dump(sub, indent + 2)
-
-my_nsid = flickr.people_findByUsername(username='offby1').find('user').attrib['nsid']
-id_to_exif_tag_to_exif_value = collections.defaultdict(dict)
+my_nsid = flickr.people_findByUsername(username='offby1')['user']['nsid']
 
 requested_page = 1
 while True:
     rsp = flickr.photos_search(user_id=my_nsid,
                                page=requested_page,
-                               per_page='10',
-                               min_upload_date=datetime.datetime(2010, 12, 1))
-    photos = rsp.find('photos')
+                               per_page='10')
 
-    if photos.get('pages') == '0':
-        print("Hmm, no photos at all.")
-        break
+    photos = rsp['photos']
 
-    print("This is page", photos.get('page'), "of", photos.get('pages'), file=sys.stderr)
-    for photo in photos:
-        id = photo.get('id')
-        print(photo.get('title'), id, "...", file=sys.stderr)
-        for exif in flickr.photos_getExif(api_key=api_key,
-                                          photo_id=id,
-                                          secret=shared_secret).find('photo').findall('exif'):
-            id_to_exif_tag_to_exif_value[id][exif.get('tag')] = exif.find('raw').text
+    print("This is page", photos['page'], "of", photos['pages'], file=sys.stderr)
+    for photo in photos['photo']:
+        id = photo['id']
+        info = flickr.photos_getInfo(photo_id=id)
+        pprint.pprint(info)
+        exit(0)
 
-    if int(photos.get('page')) >= int(photos.get('pages')):
+    if int(photos['page']) >= int(photos['pages']):
         print("That's all!")
         break
 
     requested_page += 1
-
-pprint.pprint(dict(id_to_exif_tag_to_exif_value))
-# sets = flickr.photosets_getList(user_id='73509078@N00')
