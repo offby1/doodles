3b14a44fa7c902223b5a368d0a8faf8e21b6ccac
Eric Hanchrow <eric.hanchrow@gmail.com>
2016-11-27 19:10:10 -0800

    Another way

--- python/batch.py
+++ python/batch.py
@@ -1,11 +1,17 @@
 # from http://stackoverflow.com/a/8290514/20146
 from itertools import islice, chain
 
-
 def batch(iterable, size):
     sourceiter = iter(iterable)
     while True:
         batchiter = islice(sourceiter, size)
         yield tuple(chain([next(batchiter)], batchiter))
 
+
+def batch2(iterable, size):
+    for i in range(0, len(iterable), size):
+        yield tuple(iterable[i:i+size])
+
+
 print(list(batch(range(10), 3)))
+print(list(batch2(range(10), 3)))
