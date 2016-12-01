41451b9b8b54f756bebe4e7e60fd9b2d09ac6878
Eric Hanchrow <eric.hanchrow@gmail.com>
2016-11-30 19:22:30 -0800

    Moar truth

--- autograms/python/autogram.py
+++ autograms/python/autogram.py
@@ -5,8 +5,10 @@ import string
 import num2words                # pip install num2words
 import progressbar              # pip install progressbar2
 
+ALPHABET = string.ascii_lowercase
+
 def number_and_letter_to_string(number, letter):
-    return '{} {}{}'.format(num2words.num2words(number), letter, '' if number == 1 else "'s")
+    return "{} '{}'{}".format(num2words.num2words(number), letter, '' if number == 1 else "s")
 
 
 def join_strings(strings):
@@ -16,7 +18,7 @@ def join_strings(strings):
 
 def new_counter(c):
     terse = [number_and_letter_to_string(v, k) for k, v in sorted(c.items()) if k.isalpha()]
-    string = "Can I get you " + join_strings(terse) + '?'
+    string = "Prince Phillip and Queen Elizabeth keep " + join_strings(terse) + ' at Buckingham Palace.'
 
     return collections.Counter(string.lower()), string
 
@@ -40,7 +42,7 @@ class Seen:
 
 def perturb_counter(c, seen):
     while seen(c):
-        key = random.choice(string.ascii_lowercase)
+        key = random.choice(ALPHABET)
         value = c[key]
         c[key] = value + random.randrange(-3, 4)
 
@@ -72,8 +74,10 @@ def chase_string(string):
 
     print('{} {}'.format('!!' if finished_naturally else '?', string))
 
-chase_string(string.ascii_lowercase)
+chase_string(ALPHABET)
 
 # This text contains twelve i's, seven h's, four a's, four x's, thirty-two e's, seven v's, one z, one j, eighteen n's, three d's, five y's, sixteen o's, five u's, twenty-eight s's, six w's, one q, four g's, one p, twenty t's, seven f's, four l's, one m, two c's, eight r's, one k and one b.  It really does!
 
 # This text contains four a's, one b, two c's, three d's, thirty-two e's, seven f's, four g's, seven h's, twelve i's, one j, one k, four l's, one m, eighteen n's, sixteen o's, one p, one q, eight r's, twenty-eight s's, twenty t's, five u's, seven v's, six w's, four x's, five y's and one z.  It really does!
+
+# Prince Phillip and Queen Elizabeth keep eight 'a's, three 'b's, four 'c's, three 'd's, thirty-five 'e's, nine 'f's, four 'g's, thirteen 'h's, twenty-one 'i's, one 'j', three 'k's, six 'l's, two 'm's, fifteen 'n's, eight 'o's, six 'p's, two 'q's, eleven 'r's, thirty 's's, twenty-three 't's, five 'u's, six 'v's, six 'w's, five 'x's, five 'y's and two 'z's at Buckingham Palace.
