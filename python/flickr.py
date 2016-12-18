34d4eb5a5d2faf8a412e5a6e17f954d42074d364
Eric Hanchrow <eric.hanchrow@gmail.com>
2016-12-18 10:27:32 -0800

    Grab more info per photo, including the download URLs

--- python/flickr.py
+++ python/flickr.py
@@ -14,8 +14,8 @@ import configobj                # pip install configobj
 import flickrapi                # pip install flickrapi
 import progressbar              # pip install progressbar2
 
-__author__ = 'eric.hanchrow@gmail.com'
 
+# API docs: https://www.flickr.com/services/api/
 
 def get_auth_stuff(filename=None):
     if filename is None:
@@ -25,6 +25,34 @@ def get_auth_stuff(filename=None):
 
     return(c['flickr']['api_key'],
            c['flickr']['shared_secret'])
+
+def _size_list_to_dict(getSizes_response):
+    """For mysterious reasons, the bulk of this information is a list of
+    dicts, rather than a dict keyed on the name of the size.
+
+    {u'sizes': {u'canblog': 0,
+                u'candownload': 1,
+                u'canprint': 0,
+                u'size': [{u'height': 75,
+                           u'label': u'Square',
+                           u'media': u'photo',
+                           u'source': u'https://farm9.staticflickr.com/8326/29332283952_f830b0e681_s.jpg',
+                           u'url': u'https://www.flickr.com/photos/offby1/29332283952/sizes/sq/',
+                           u'width': 75},
+                          {u'height': u'150',
+                           u'label': u'Large Square',
+                           u'media': u'photo',
+                           u'source': u'https://farm9.staticflickr.com/8326/29332283952_f830b0e681_q.jpg',
+                           u'url': u'https://www.flickr.com/photos/offby1/29332283952/sizes/q/',
+                           u'width': u'150'}]},
+     u'stat': u'ok'}
+    """
+    rv = {}
+    for d in getSizes_response['sizes']['size']:
+        rv[d['label']] = d
+
+    return {'sizes': rv}
+
 
 api_key, shared_secret = get_auth_stuff()
 
@@ -53,11 +81,14 @@ with progressbar.ProgressBar() as bar:
 
             for index, photo in enumerate(photos['photo']):
                 id = photo['id']
-                info = flickr.photos_getInfo(photo_id=id)
-                pprint.pprint(info)
+                blob = {}
+                blob.update(flickr.photos_getExif(photo_id=id)['photo'])
+                blob.update(flickr.photos_getInfo(photo_id=id)['photo'])
+                blob.update(_size_list_to_dict(flickr.photos_getSizes(photo_id=id)))
+                pprint.pprint(blob)
                 bar.update(per_page * (int(photos['page']) - 1) + index)
 
-            if int(photos['page']) >= int(photos['pages']):
+            if int(photos['page']) == int(photos['pages']):
                 print("That's all!")
                 break
 
