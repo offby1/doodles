4359f8c07b22153aed1d9acd413c75b8b6e727c6
Eric Hanchrow <eric.hanchrow@gmail.com>
2017-01-13 07:26:27 -0800

    Two new puzzlers from fivethirtyeight.com

---
+++ python/birthday-candles.py
@@ -0,0 +1,18 @@
+# https://fivethirtyeight.com/features/how-long-will-it-take-to-blow-out-the-birthday-candles/
+
+import random
+import statistics
+
+def trial(number_burning=30):
+    blows = 0
+    while (number_burning > 0):
+        n = random.randint(1, number_burning)
+        number_burning -= n
+
+        blows += 1
+
+    return blows
+
+data = [trial() for _ in range(10000)]
+print ('Mean : {}'.format(statistics.mean(data)))
+print ('Stdev: {}'.format(statistics.stdev(data)))
---
+++ python/card-deal.py
@@ -0,0 +1,16 @@
+# http://fivethirtyeight.com/features/can-you-deal-with-these-card-game-puzzles/
+
+import itertools
+import random
+
+def trial():
+    deck = list(range(52))
+    random.shuffle(deck)
+    for i, c in enumerate(deck):
+        if c == i % 4:
+            return False
+
+    return True
+
+if __name__ == "__main__":
+    print (sum([trial() for t in range(10000)]))
