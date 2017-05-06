b095bd852be67fafc22009e94679096e0a13ecfd
Eric Hanchrow <eric.hanchrow@gmail.com>
2017-05-06 10:08:58 -0700

    Parent and kid

--- python/curio/hello.py
+++ python/curio/hello.py
@@ -1,3 +1,4 @@
+# hello.py
 import curio
 
 async def countdown(n):
@@ -6,5 +7,21 @@ async def countdown(n):
         await curio.sleep(1)
         n -= 1
 
+async def kid():
+    print('Building the Millenium Falcon in Minecraft')
+    await curio.sleep(1000)
+
+async def parent():
+    kid_task = await curio.spawn(kid)
+    await curio.sleep(5)
+
+    print("Let's go")
+    count_task = await curio.spawn(countdown, 10)
+    await count_task.join()
+
+    print("We're leaving!")
+    await kid_task.join()
+    print('Leaving')
+
 if __name__ == '__main__':
-    curio.run(countdown, 10)
+    curio.run(parent, with_monitor=True)
