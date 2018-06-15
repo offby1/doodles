e1349a325ad817e5eed2bd718515411150abeb22
Eric Hanchrow <erichan@amazon.com>
2018-06-15 12:54:07 -0700

    Make it work with python2

--- python/styles-of-reraised-exceptions.py
+++ python/styles-of-reraised-exceptions.py
@@ -1,4 +1,4 @@
-from enum import Enum, auto
+from enum import Enum
 import logging
 
 logging.basicConfig()
@@ -6,10 +6,10 @@ logging.basicConfig()
 _log = logging.getLogger()
 
 class RaiseStyle(Enum):
-    BARE = auto()
-    RERAISE = auto()
-    NEW = auto()
-    FROM = auto()
+    BARE    = 0
+    FROM    = 1
+    NEW     = 2
+    RERAISE = 3
 
 def top():
     print("Hi I'm a top-level function")
@@ -36,9 +36,9 @@ def caller(raise_style):
         elif raise_style == RaiseStyle.NEW:
             print("Totally new exception")
             raise Exception("Unrelated to what I just caught")
-        elif raise_style == RaiseStyle.FROM:
-            print("`From' style")
-            raise Exception("Unrelated to what I just caught") from e
+        # elif raise_style == RaiseStyle.FROM:
+        #     print("`From' style")
+        #     raise Exception("Unrelated to what I just caught") from e
 
 
 if __name__ == "__main__":
@@ -46,4 +46,4 @@ if __name__ == "__main__":
         try:
             caller(raise_style=style)
         except Exception:
-            _log.exception(f"That was {style}")
+            _log.exception("That was {}".format(style))
