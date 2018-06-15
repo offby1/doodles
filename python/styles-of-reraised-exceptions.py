9cd5b2494c96bbbeebe284be82dfeb8c948b9181
Eric Hanchrow <erichan@amazon.com>
2018-06-15 09:50:59 -0700

    Survey ways to reraise an exception

---
+++ python/styles-of-reraised-exceptions.py
@@ -0,0 +1,49 @@
+from enum import Enum, auto
+import logging
+
+logging.basicConfig()
+
+_log = logging.getLogger()
+
+class RaiseStyle(Enum):
+    BARE = auto()
+    RERAISE = auto()
+    NEW = auto()
+    FROM = auto()
+
+def top():
+    print("Hi I'm a top-level function")
+    middle()
+
+def middle():
+    print("MIddle guy here")
+    inner()
+
+def inner():
+    print("Inner")
+    raise Exception("Good golly miss MOlly")
+
+def caller(raise_style):
+    try:
+        top()
+    except Exception as e:
+        if raise_style == RaiseStyle.RERAISE:
+            print("Raise e")
+            raise e
+        elif raise_style == RaiseStyle.BARE:
+            print("Bare raise")
+            raise
+        elif raise_style == RaiseStyle.NEW:
+            print("Totally new exception")
+            raise Exception("Unrelated to what I just caught")
+        elif raise_style == RaiseStyle.FROM:
+            print("`From' style")
+            raise Exception("Unrelated to what I just caught") from e
+
+
+if __name__ == "__main__":
+    for style in RaiseStyle:
+        try:
+            caller(raise_style=style)
+        except Exception:
+            _log.exception(f"That was {style}")
