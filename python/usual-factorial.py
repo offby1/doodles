8ec9791bdde99b7c937765b68b424afa76366220
Eric Hanchrow <eric.hanchrow@gmail.com>
2018-10-06 23:15:15 +0000

    Surprised I didn't already have these lying around:
    - factorial
    - half-or-triple-plus-one

---
+++ python/usual-factorial.py
@@ -0,0 +1,30 @@
+import functools
+import itertools
+
+
+def memoize(func):
+    cache = {}
+
+    @functools.wraps(func)
+    def _inner(arg):
+        if arg in cache:
+            return cache[arg]
+
+        result = func(arg)
+        cache[arg] = result
+        return result
+    return _inner
+
+
+@memoize
+def fact(n):
+    n = abs(int(n))
+    if n <= 1:
+        return 1
+    return n * fact(n - 1)
+
+
+sum = 0
+for n in itertools.count(1):
+    sum += 1 / fact(n)
+    print(sum)
