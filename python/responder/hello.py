5ce6c6a9a574f8cd55d09a39727aa4867954c1d8
Eric Hanchrow <eric.hanchrow@gmail.com>
2018-10-28 12:08:14 -0700

    Responder.
    Created pipfiles with
    
    > pipenv install responder
    
    which emitted an error about inconsistent whatsahoozies; mollified that by
    
    > pipenv lock --pre
    
    Templates and static files seem to work

---
+++ python/responder/hello.py
@@ -0,0 +1,21 @@
+import logging
+import responder
+
+api = responder.API()
+
+@api.route("/{greeting}")
+async def greet_world(req, resp, *, greeting):
+    resp.text = f"{greeting}, world!"
+
+@api.route("/dumb")
+async def dumb(req, resp):
+    resp.content = api.template('dumb.html', variable='wobbly')
+
+if __name__ == '__main__':
+    # fix logging to be more like RFC3339
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s %(levelname)-5.5s [%(name)s][%(threadName)s] %(message)s')
+    logging.Formatter.default_time_format = '%FT%T'
+    logging.Formatter.default_msec_format = '%s.%03dZ'
+
+    api.run()
