cf7d28c4522c3cc750cdec0d89da8c6bcb55b016
Eric Hanchrow <eric.hanchrow@gmail.com>
2018-12-09 13:38:22 -0800

    Tweak tree traverser to return the "path"

--- python/tree.py
+++ python/tree.py
@@ -1,4 +1,12 @@
+"""
+Test me like this
+
+    python3 -m pytest tree.py
+
+"""
+
 import random
+
 
 class Tree:
     def __init__(self):
@@ -30,32 +38,67 @@ class Tree:
                 return self
             return self.left.find_parent_for(item)
 
-    def traverse_depth_first(self):
+    def traverse_depth_first(self, path_to_here=None):
+        """
+        Generator that emits one node at a time
+        """
+        if path_to_here is None:
+            path_to_here = []
+
+        path_to_here = path_to_here + [self]
+
         if self.item is None:
             return
         if self.left:
-            for child in self.left.traverse_depth_first():
-                yield child
-        yield self
+            for child, left_path_to_here in self.left.traverse_depth_first(path_to_here):
+                yield (child, left_path_to_here)
+        yield (self, path_to_here)
         if self.right:
-            for child in self.right.traverse_depth_first():
-                yield child
+            for child, right_path_to_here in self.right.traverse_depth_first(path_to_here):
+                yield (child, right_path_to_here)
+
 
 def test_it():
     t = Tree()
     for i in ['cat', 'dog', 'aardvark']:
         t.add_item(i)
 
-    assert [n.item[0] for n in t.traverse_depth_first()] == ['aardvark', 'cat', 'dog']
+    assert [n.item[0] for (n, _) in t.traverse_depth_first()] == ['aardvark', 'cat', 'dog']
+
 
 def test_it_some_more():
     shuffled = list(range(100))
     random.shuffle(shuffled)
-    print(shuffled)
 
     t = Tree()
     for i in shuffled:
         t.add_item(i)
 
-    ordered = [n.item[0] for n in t.traverse_depth_first()]
+    ordered = [n.item[0] for (n, _) in t.traverse_depth_first()]
     assert ordered == list(range(100))
+
+
+def test_returned_paths():
+    """
+     cat
+     /   \
+aardvark  \ dog
+           /     \
+      clydesdale   elephant
+    """
+
+    t = Tree()
+    for i in ['cat', 'aardvark', 'dog', 'clydesdale', 'elephant']:
+        t.add_item(i)
+
+    paths_by_node = {}
+    for (node, path) in t.traverse_depth_first():
+        [key] = node.item
+        value = [n.item for n in path]
+        paths_by_node[key] = value
+
+    assert paths_by_node['aardvark']   == [['cat'], ['aardvark']]
+    assert paths_by_node['cat']        == [['cat']]
+    assert paths_by_node['clydesdale'] == [['cat'], ['dog'], ['clydesdale']]
+    assert paths_by_node['dog']        == [['cat'], ['dog']]
+    assert paths_by_node['elephant']   == [['cat'], ['dog'], ['elephant']]
