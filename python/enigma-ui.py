b1729e205f86e1a643da3d589adf0f0dd96a0244
Eric Hanchrow <eric.hanchrow@gmail.com>
2019-12-28 17:53:46 -0800

    save the output to a file named for the input
    reformat with "black"

--- python/enigma-ui.py
+++ python/enigma-ui.py
@@ -1,8 +1,11 @@
 from curses import wrapper
+import pathlib
 import random
+import tempfile
 import time
 
 from enigma import Enigma
+
 
 class LetterLocator:
     def __init__(self):
@@ -16,7 +19,6 @@ class LetterLocator:
             "  z x c v b n m",
         ]
 
-
         for row_index, row_letters in enumerate(keyboard_layout):
             for column_index, letter in enumerate(row_letters):
                 self.locations_by_letter[letter] = (row_index, column_index)
@@ -25,39 +27,48 @@ class LetterLocator:
         return self.locations_by_letter.get(l)
 
 
-def main(stdscr):
+def main(stdscr, transcript_binary_fh):
     stdscr.clear()
 
     stdscr.addstr(0, 0, "Hit ! to exit")
     stdscr.refresh()
 
+    plaintext = []
     e = Enigma()
-
     locator = LetterLocator()
     last_lit_location = None
 
     while True:
         k = stdscr.getkey()
 
-        if k == '!':
+        if k == "!":
             break
 
+        plaintext.append(k)
         raw_encrypted = e.encrypt_single_number(ord(k))
         if raw_encrypted:
+            transcript_binary_fh.write(chr(raw_encrypted).encode("latin-1"))
             encrypted = chr(raw_encrypted)
 
-            loc = locator(k)
+            loc = locator(encrypted)
             if loc:
 
                 if last_lit_location is not None:
-                    stdscr.addstr(*last_lit_location, ' ')
+                    stdscr.addstr(*last_lit_location, " ")
                     stdscr.refresh()
 
                 stdscr.addstr(*loc, encrypted)
                 stdscr.refresh()
                 last_lit_location = loc
 
+    transcript_binary_fh.write(b"\n")
+    return plaintext
+
 
 if __name__ == "__main__":
     random.seed(0)
-    wrapper(main)
+    with tempfile.NamedTemporaryFile(delete=False) as transcript:
+        plaintext = "".join(wrapper(main, transcript))
+        final_name = pathlib.Path("/tmp") / plaintext
+    pathlib.Path(transcript.name).rename(final_name)
+    print(f"Your encryption/decryption of {plaintext!r} is in {final_name}")
