6c5d8c1380ea96506de20a1eb39a8e4c419c5571
Eric Hanchrow <eric.hanchrow@gmail.com>
2020-01-28 09:47:12 -0800

    Mo' data

--- python/wind-weather/darksky.py
+++ python/wind-weather/darksky.py
@@ -75,7 +75,7 @@ if __name__ == '__main__':
     writer = csv.writer (sys.stdout)
     writer.writerow (('time', 'cloudCover', 'windBearing'))
 
-    for one_hours_data in tqdm.tqdm(one_years_hourly_data (2018), total=365 * 24):
+    for one_hours_data in tqdm.tqdm(one_years_hourly_data (2019), total=365 * 24):
         hour = format_timestamp(one_hours_data[0])
         one_hours_data = tuple([hour]) + one_hours_data[1:]
         writer.writerow (one_hours_data)
