f4c9b3268043c6c5073fee1a18d85dc04ead06cb
Eric Hanchrow <eric.hanchrow@gmail.com>
2020-02-01 16:37:28 -0800

    arguably simpler

--- python/ciphers/another-simple-cipher.py
+++ python/ciphers/another-simple-cipher.py
@@ -1,36 +1,26 @@
 import itertools
-import secrets
+import random
+import string
 
-RANDOM_ARRAY = [secrets.randbelow(256) for _ in range(64)]
+lets = string.printable
 
+scrambled = list(lets)
+random.shuffle(scrambled)
+scrambled = ''.join(scrambled)
 
-def text_to_numbers(text):
-    return list(text.encode('utf-8'))
+forward_mapping = dict(zip(lets, scrambled))
+reverse_mapping = dict(zip(scrambled, lets))
 
-
-def numbers_to_text(nums):
-    return bytes(nums).decode('utf-8')
-
-
-def _encrypt_number(plaintext, key):
-    return (plaintext + key) % 256
-
-
-def _decrypt_number(ciphertext, key):
-    return (ciphertext - key) % 256
+def transform(letter, encrypt=True):
+    map_ = forward_mapping if encrypt else reverse_mapping
+    return map_.get(letter, letter)
 
 
-def mangle_numbers(nums, encrypt=True):
-    func = _encrypt_number if encrypt else _decrypt_number
-    for plaintext_number, key_number in zip(nums, itertools.cycle(RANDOM_ARRAY)):
-        yield func(plaintext_number, key_number)
-
-
-with open ('/Users/erichanchrow/git-repositories/3rd-party/emacs/nextstep/Emacs.app/Contents/Resources/etc/HELLO') as inf:
+with open ('/etc/passwd') as inf:
     more_sample_text = inf.read()
     with open('/tmp/decrypt-me', 'w') as outf:
-        outf.buffer.write(bytes(mangle_numbers(text_to_numbers(more_sample_text))))
+        outf.write(''.join(transform(l) for l in more_sample_text))
         print(f'Check out {outf.name!r} for some ciphertext')
 
-    with open('/tmp/decrypt-me', 'rb') as inf:
-        print(numbers_to_text(mangle_numbers(inf.read(), encrypt=False)))
+    with open('/tmp/decrypt-me', 'r') as inf:
+        print(''.join(transform(l, encrypt=False) for l in inf.read()))
