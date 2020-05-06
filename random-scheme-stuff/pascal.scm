9d676c635b8a3b0e46c771e87f0235d4841ea87f
Eric Hanchrow <eric.hanchrow@gmail.com>
2020-05-05 20:46:34 -0700

    cosmetix

--- random-scheme-stuff/pascal.scm
+++ random-scheme-stuff/pascal.scm
@@ -1,3 +1,5 @@
+#lang scheme
+
 ; To make a pascal's triangle of order 0, return ((1)) .
 
 ; To make a pascal's triangle of order n > 0, return pascal's triangle
@@ -8,22 +10,22 @@
 
   (define (new-row old-row)
     (let loop ((result (list (car old-row)))
-	       (old-row old-row))
+               (old-row old-row))
       (cond ((null? old-row)
-	     result)
-	    ((= (length old-row)
-		1)
-	     (append result (list (car old-row))))
-	    (#t (loop (append result (list (+ (car old-row) (cadr old-row))))
-		      (cdr old-row))))))
+             result)
+            ((= (length old-row)
+                1)
+             (append result (list (car old-row))))
+            (#t (loop (append result (list (+ (car old-row) (cadr old-row))))
+                      (cdr old-row))))))
 
   (define (last list)
     (if (null? (cdr list))
-	(car list)
+        (car list)
       (last (cdr list))))
 
   (if (>= 0 order)
       '((1))
     (let ((smaller-triangle (pascal (- order 1))))
       (append smaller-triangle
-	      (list (new-row (last smaller-triangle)))))))
+              (list (new-row (last smaller-triangle)))))))
