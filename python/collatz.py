2b885e68afc84bd03cf665ea91445d7817bdb73d
Eric Hanchrow <eric.hanchrow@gmail.com>
2020-09-22 07:34:42 -0700

    Fancify it with, like, statistics 'n' stuff

--- python/collatz.py
+++ python/collatz.py
@@ -1,14 +1,9 @@
 # https://en.wikipedia.org/wiki/3x_%2B_1_problem
+import functools
 import itertools
 
 
-def repeated_calls(function, argument):
-    result = argument
-    while True:
-        yield result
-        result = function(result)
-
-
+@functools.lru_cache(maxsize=None)
 def hotpo(x):
     q, r = divmod(x, 2)
     if r == 0:
@@ -17,4 +12,31 @@ def hotpo(x):
         return 1 + 3 * x
 
 
-print(list(itertools.islice(repeated_calls(hotpo, 27), 111)))
+def cycle_length(starting_value):
+    seen_values = set()
+    for index in itertools.count():
+        if starting_value in seen_values:
+            return index
+        seen_values.add(starting_value)
+        starting_value = hotpo(starting_value)
+
+
+def find_big_cyles():
+    successively_longer_cycles = []
+    for starting_value in itertools.count():
+        toople = starting_value, cycle_length(starting_value)
+
+        if not successively_longer_cycles:
+            successively_longer_cycles = [toople]
+
+        if toople[1] > successively_longer_cycles[-1][1]:
+            successively_longer_cycles.append(toople)
+            print(successively_longer_cycles[-1])
+
+        if len(successively_longer_cycles) == 35:
+            break
+
+
+if __name__ == "__main__":
+    find_big_cyles()
+    print(hotpo.cache_info())
