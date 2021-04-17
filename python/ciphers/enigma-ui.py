fa63a8d825d2b411b5b632b3e133560ff7c65612
Eric Hanchrow <eric.hanchrow@gmail.com>
2021-04-17 07:37:08 -0700

    cosmetics, including nice big fonts!!

--- python/ciphers/enigma-ui.py
+++ python/ciphers/enigma-ui.py
@@ -1,7 +1,9 @@
 import random
-from tkinter import E, EventType, N, S, Tk, W, ttk
+from tkinter import E, EventType, N, S, Tk, W, font, ttk
 
 from enigma import Enigma
+
+labels_by_letter = {}
 
 
 def on_key_press(*events):
@@ -31,25 +33,23 @@ def on_key_release(*events):
             label.configure(foreground='black')
 
 
-labels_by_letter = {}
-
-
-def add_keyboard(parent_window):
+def add_letter_displays(parent_window):
     def generic_label(column_index, row_index, text):
-        label = ttk.Label(parent_window, text=text)
+        label = ttk.Label(parent_window, text=text, font=the_font)
         label.grid(
             column=column_index,
             row=row_index,
             sticky=(N, S, E, W),
         )
-        label.configure(
-            foreground='black'
-        )
+        label.configure(foreground='black')
         parent_window.columnconfigure(column_index, weight=1)
         parent_window.rowconfigure(row_index, weight=1)
-        return label
 
-    keyboard_layout = [
+        labels_by_letter[letter] = label
+        root.bind(f'<KeyPress-{letter}>', lambda e: on_key_press(e))
+        root.bind(f'<KeyRelease-{letter}>', lambda e: on_key_release(e))
+
+    display_layout = [
         "q w e r t y u i o p",
         " a s d f g h j k l",
         "  z x c v b n m",
@@ -57,28 +57,22 @@ def add_keyboard(parent_window):
 
     letters_to_place = set(enigma.alphabet)
 
-    for row_index, row_letters in enumerate(keyboard_layout):
+    for row_index, row_letters in enumerate(display_layout):
         for column_index, letter in enumerate(row_letters):
 
             if letter == ' ':
                 continue
 
-            label = generic_label(column_index, row_index, letter)
-            labels_by_letter[letter] = label
-            root.bind(f'<KeyPress-{letter}>', lambda e: on_key_press(e))
-            root.bind(f'<KeyRelease-{letter}>', lambda e: on_key_release(e))
+            generic_label(column_index, row_index, letter)
 
             letters_to_place.remove(letter)
 
     for index, letter in enumerate(sorted(letters_to_place)):
-        label = generic_label(column_index, row_index + 1, '_' if letter == ' ' else letter)
-        labels_by_letter[letter] = label
-        root.bind(f'<KeyPress-{letter}>', lambda e: on_key_press(e))
-        root.bind(f'<KeyRelease-{letter}>', lambda e: on_key_release(e))
+        generic_label(column_index, row_index + 1, '_' if letter == ' ' else letter)
 
 
 def on_output_label_Destroy(e):
-    # printing to stdout seems not to work; I assume the event loop has done something weird with it
+
     with open('ciphertext', 'w') as outf:
         print(ciphertext, file=outf)
         print(f"Wrote {outf.name}")
@@ -91,23 +85,24 @@ ciphertext = ''
 root = Tk()
 root.title("Enigma Encryptor")
 
+the_font = font.nametofont('TkFixedFont')
+the_font.configure(size=75)
+
 input_frame = ttk.Frame(root)
 input_frame.grid(column=0, row=0, sticky=(N, W, E, S))
 
 output_frame = ttk.Frame(root)
 output_frame.grid(column=0, row=1, sticky=(W, S))
 
-plaintext_label = ttk.Label(output_frame, text='')
+plaintext_label = ttk.Label(output_frame, text='', font=the_font)
 plaintext_label.grid(column=0, row=0, sticky=(W, S))
-plaintext_label['font'] = 'TkFixedFont'
 
-ciphertext_label = ttk.Label(output_frame, text='')
+ciphertext_label = ttk.Label(output_frame, text='', font=the_font)
 ciphertext_label.grid(column=0, row=1, sticky=(W, S))
-ciphertext_label['font'] = 'TkFixedFont'
 ciphertext_label.bind('<Destroy>', on_output_label_Destroy)
 
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
-add_keyboard(input_frame)
+add_letter_displays(input_frame)
 root.mainloop()
