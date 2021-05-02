b37b191dae3ddd3e5bd5225c41fb8dbf2b8fb19d
Eric Hanchrow <eric.hanchrow@gmail.com>
2021-05-01 19:49:05 -0700

    Get my name outta the code

--- python/wind-weather/draw-graph.py
+++ python/wind-weather/draw-graph.py
@@ -1,7 +1,9 @@
+import pathlib
+
 import matplotlib.pyplot as plt
 import pandas as pd
 
-df = pd.read_csv('/Users/erichanchrow/git-repositories/me/doodles/python/wind-weather/2020-data.csv')
+df = pd.read_csv(pathlib.Path('~/git-repositories/me/doodles/python/wind-weather/2020-data.csv').expanduser())
 df[['cloudCover', 'windBearing']].plot(y='cloudCover', x='windBearing', kind='scatter')
 
 plt.show()
