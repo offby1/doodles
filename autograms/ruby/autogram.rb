ccec3c3496088cff829aaf7c2ca606f6254c4189
Eric Hanchrow <eric.hanchrow@gmail.com>
2021-05-13 16:14:23 -0700

    Sorta works, fsvo "works"

--- autograms/ruby/autogram.rb
+++ autograms/ruby/autogram.rb
@@ -1,24 +1,30 @@
-#!/usr/bin/env ruby1.9
+require 'set'
+require 'pp'
+
+# "gem install" these
+require 'pluralizer'
+require 'spell_number'
 
 def histogram(string)
   lettermap = Hash.new{|h,k|h[k] = 0}
   string.chars.each do |c|
-    lettermap[c] += 1
+    lettermap[c.to_sym] += 1
   end
+
   lettermap
 end
 
-def expand_template(letters, template)
-  histogram(template)
+def update_histogram(template, letters)
+  expanded = template % letters
 end
 
-def update_histogram(letters, template)
-  next_histogram = expand_template(letters, template)
+template = "This sentence has %{a} a's and %{q} qs."
+h = histogram('')
 
-  letters.each do |letter, count|
-    puts "#{letter}: #{count} => #{next_histogram[letter]}"
-  end
-end
-
-h = histogram("This sentence has yadda yadda.")
-update_histogram(h, "This sentence has yadda yadda.")
+seen = Set[h]
+loop {
+  h = histogram(template % h)
+  break if seen === h
+  puts template % h
+  seen.add(h)
+}
