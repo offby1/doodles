0c2107b636e762f9f78fe06462d827d7525ca9e4
Eric Hanchrow <eric.hanchrow@gmail.com>
2021-05-13 19:19:39 -0700

    Gotta have a progress bar!

--- autograms/ruby/autogram.rb
+++ autograms/ruby/autogram.rb
@@ -2,7 +2,9 @@ require 'set'
 require 'pp'
 
 require 'active_support/core_ext/string/inflections' # gem install rails
-require 'spell_number'# gem install spell_number
+require 'spell_number'                               # gem install spell_number
+require 'ruby-progressbar'      # gem install ruby-progressbar
+
 
 def histogram(string)
   lettermap = Hash.new(0)
@@ -13,8 +15,14 @@ def histogram(string)
   lettermap
 end
 
-def update_histogram(template, letters)
-  expanded = template % letters
+def randomize_histogram(h, max)
+  new = Hash.new(0)
+  h.each {
+    |k, v|
+
+    new[k] = Random.rand(max)
+  }
+  new
 end
 
 def render(template, histogram)
@@ -29,15 +37,32 @@ def render(template, histogram)
   template % new
 end
 
-template = "This sentence has %{a} and %{q}."
+template = ("This sentence has %{a}, %{b}, %{c}, %{d}, %{e}, %{f}, %{g}, %{h}, %{i}, %{j}, %{k}, %{l}, %{m}, " +
+            "%{n}, %{o}, %{p}, %{q}, %{r}, %{s}, %{t}, %{u}, %{v}, %{w}, %{x}, %{y}, and %{z}.")
+
 h = histogram(template)
 
 seen = Set[]
+last = nil
+progressbar = ProgressBar.create(
+  :total => nil,
+  :title => "Attempts",
+  :format => "%t: %c; %a",
+)
 loop {
   rendered = render(template, h)
-  break if seen === rendered
-  seen.add(rendered)
-  puts rendered
-  h = histogram(rendered)
-}
+  if last == rendered
+    puts "Hooray, truth"
+    puts rendered
+    break
+  end
+  last = rendered
+  if seen === rendered
+    h = randomize_histogram(h, rendered.length())
+  else
+    seen.add(rendered)
+    h = histogram(rendered)
 
+    progressbar.increment
+  end
+}
