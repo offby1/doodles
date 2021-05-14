f7faddfda38750774b59f45c69c156ea695006d9
Eric Hanchrow <eric.hanchrow@gmail.com>
2021-05-13 17:21:23 -0700

    Now we're cookin' with gas.

--- autograms/ruby/autogram.rb
+++ autograms/ruby/autogram.rb
@@ -1,14 +1,13 @@
 require 'set'
 require 'pp'
 
-# "gem install" these
-require 'pluralizer'
-require 'spell_number'
+require 'active_support/core_ext/string/inflections' # gem install rails
+require 'spell_number'# gem install spell_number
 
 def histogram(string)
-  lettermap = Hash.new{|h,k|h[k] = 0}
+  lettermap = Hash.new(0)
   string.chars.each do |c|
-    lettermap[c.to_sym] += 1
+    lettermap[c] += 1
   end
 
   lettermap
@@ -18,14 +17,27 @@ def update_histogram(template, letters)
   expanded = template % letters
 end
 
-template = "This sentence has %{a} a's and %{q} qs."
-h = histogram('')
+def render(template, histogram)
+  new = Hash.new(0)
+  histogram.each {
+    |k, v|
+
+    k = 'space' if k == ' '
+    new[k.to_sym] = "#{SpellNumber.number_to_words(v)} #{k.pluralize(v)}"
+  }
+
+  template % new
+end
+
+template = "This sentence has %{a} and %{q}."
+h = histogram(template)
 
 seen = Set[]
 loop {
-  rendered = template % h
+  rendered = render(template, h)
   break if seen === rendered
   seen.add(rendered)
   puts rendered
   h = histogram(rendered)
 }
+
