58a5c11117dd5d6befcb801642accb7af33a2890
Eric Hanchrow <eric.hanchrow@gmail.com>
2021-08-11 18:36:39 -0700

    Use math.gcd instead of fractions.gcd
    I think some warning said that the latter was deprecated.

--- python/prime-pump.py
+++ python/prime-pump.py
@@ -1,13 +1,13 @@
 # http://bit-player.org/2015/pumping-the-primes
 
-import fractions
+import math
 
 
 def lotta_primes():
     n = 2
     a = 7
     while True:
-        g = fractions.gcd(n, a)
+        g = math.gcd(n, a)
         n = n + 1
         a = a + g
         if g != 1:
