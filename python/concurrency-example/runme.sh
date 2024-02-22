7095602ce9a24ea5c345c2ebeb5f090a4e8c7d3b
Eric Hanchrow <eric.hanchrow@gmail.com>
2024-02-21 18:49:43 -0800

    Simplify

--- python/concurrency-example/runme.sh
+++ python/concurrency-example/runme.sh
@@ -5,16 +5,8 @@ set -x
 
 cd "${here}"
 
-if ! python3 -c 'import poetry'
-then
-    echo "You don't have \`poetry\`, so I'll install it for you"
-    python3 -m pip install --user --no-input poetry
-fi
+poetry install --no-root
 
-if ! [ -x $here/.venv/bin/python3 ]
-then
-   python3 -m poetry install
-fi
 
 set -e
 
